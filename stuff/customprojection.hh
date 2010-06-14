9af130b5ddafee5493dd6130add13d690dcb2531
Rene Milk <rene.milk@uni-muenster.de>
2010-06-14 14:22:47 +0200

    implement an L2 projection that actually works

--- stuff/customprojection.hh
+++ stuff/customprojection.hh
@@ -84,6 +84,95 @@ public:
 		}
 	}
 };
-
 }//end namespace Stuff
+
+namespace Dune {
+class BetterL2Projection {
+public:
+	template <class FunctionImp, class DiscreteFunctionImp>
+	static void project(const FunctionImp& func,
+								DiscreteFunctionImp& discFunc,
+								int polOrd = -1)
+	{
+	  typedef typename DiscreteFunctionImp::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+	  typedef typename DiscreteFunctionImp::LocalFunctionType LocalFuncType;
+	  typedef typename DiscreteFunctionSpaceType::Traits::GridPartType GridPartType;
+	  typedef typename DiscreteFunctionSpaceType::Traits::IteratorType Iterator;
+	  typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType BaseFunctionSetType ;
+	  typedef typename GridPartType::GridType GridType;
+
+	  typedef typename FunctionImp::LocalFunctionType LocalFType;
+
+	  typename DiscreteFunctionSpaceType::RangeType ret (0.0);
+	  typename DiscreteFunctionSpaceType::RangeType phi (0.0);
+	  const DiscreteFunctionSpaceType& space =  discFunc.space();
+
+	  // type of quadrature
+	  typedef CachingQuadrature<GridPartType,0> QuadratureType;
+	  // type of local mass matrix
+	  typedef LocalDGMassMatrix< DiscreteFunctionSpaceType, QuadratureType > LocalMassMatrixType;
+
+	  const int quadOrd = (polOrd == -1) ? (2 * space.order()) : polOrd;
+
+	  // create local mass matrix object
+	  LocalMassMatrixType massMatrix( space, quadOrd );
+
+	  // check whether geometry mappings are affine or not
+	  const bool affineMapping = massMatrix.affine();
+
+	  // clear destination
+	  discFunc.clear();
+
+	  const Iterator endit = space.end();
+	  for(Iterator it = space.begin(); it != endit ; ++it)
+	  {
+		// get entity
+		const typename GridType::template Codim<0>::Entity& en = *it;
+		// get geometry
+		const typename GridType::template Codim<0>::Geometry& geo = en.geometry();
+
+		// get quadrature
+		QuadratureType quad(en, quadOrd);
+
+		// get local function of destination
+		LocalFuncType lf = discFunc.localFunction(en);
+		// get local function of argument
+		const LocalFType f = func.localFunction(en);
+
+		// get base function set
+		const BaseFunctionSetType & baseset = lf.baseFunctionSet();
+
+		const int quadNop = quad.nop();
+		const int numDofs = lf.numDofs();
+
+		for(int qP = 0; qP < quadNop ; ++qP)
+		{
+		  const double intel = (affineMapping) ?
+			   quad.weight(qP) : // affine case
+			   quad.weight(qP) * geo.integrationElement( quad.point(qP) ); // general case
+
+		  // evaluate function
+		  typename FunctionImp::DiscreteFunctionSpaceType::RangeType
+				  dummy;
+		  f.evaluate(quad[qP], dummy);
+		  ret =dummy;
+
+		  // do projection
+		  for(int i=0; i<numDofs; ++i)
+		  {
+			baseset.evaluate(i, quad[qP], phi);
+			lf[i] += intel * (ret * phi) ;
+		  }
+		}
+
+		// in case of non-linear mapping apply inverse
+		if ( ! affineMapping )
+		{
+		  massMatrix.applyInverse( en, lf );
+		}
+	  }
+	}
+};
+
+}//end namespace Dune
 #endif // CUSTOMPROJECTION_HH
