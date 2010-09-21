2e2222e2e6cf137bf23d50678ff8ae554c1b3265
Rene Milk <rene.milk@uni-muenster.de>
2010-09-21 22:20:31 +0200

    new runs script for alpha/mu vary

---
+++ scripts/alpha_mu_runs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import math,sys,os,subprocess
+
+power = 6
+binary = sys.argv[1]
+param = sys.argv[2]
+#null = sys.stdout
+null = open(os.devnull, 'w')
+datadir = filter( lambda f: f.startswith( 'fem.io.datadir' ), open(param).readlines() )[0].split(':')[1].strip()
+
+for alpha_power in range(-power,power+1): 
+	for mu_power in range(-power,power+1):
+		cmd = [ './%s'%binary, 'paramfile:%s'%param, 'viscosity:%f'%math.pow(10,mu_power), 'alpha:%f'%math.pow(10,alpha_power) ]
+		subprocess.call(cmd, bufsize=4096, stdout=null, stderr=null)
+		cmd = ['cat','%s/eoc-file_body.tex >> %s/all.tex'%(datadir,datadir )]
+		ko = open('%s/all.tex'%datadir, 'a' )
+		ko.write( open('%s/eoc-file_body.tex'%datadir).read() )
+		ko.close()
