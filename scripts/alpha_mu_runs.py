9c9d2bada5d0545fac8c4b68c66e8865109aa9f7
Rene Milk <rene.milk@uni-muenster.de>
2010-09-22 09:49:59 +0200

    alpha mu to output datetime

--- scripts/alpha_mu_runs.py
+++ scripts/alpha_mu_runs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import math,sys,os,subprocess
+import math,sys,os,subprocess,datetime
 
 power = 6
 binary = sys.argv[1]
@@ -12,8 +12,8 @@ datadir = filter( lambda f: f.startswith( 'fem.io.datadir' ), open(param).readli
 for alpha_power in range(-power,power+1): 
 	for mu_power in range(-power,power+1):
 		cmd = [ './%s'%binary, 'paramfile:%s'%param, 'viscosity:%f'%math.pow(10,mu_power), 'alpha:%f'%math.pow(10,alpha_power) ]
+		print datetime.datetime.now(),cmd
 		subprocess.call(cmd, bufsize=4096, stdout=null, stderr=null)
-		cmd = ['cat','%s/eoc-file_body.tex >> %s/all.tex'%(datadir,datadir )]
 		ko = open('%s/all.tex'%datadir, 'a' )
 		ko.write( open('%s/eoc-file_body.tex'%datadir).read() )
 		ko.close()
