wwu-numerik/dune-stuff

commit 96aa28c4e3ae13e07234834e8bf5bb41480f6e6e

Fixed a deprecation warning (use array instead of fixedarray)

stuff/misc.hh
@@ -9,7 +9,7 @@
 #include <map>
 #include <assert.h>
 #include <algorithm>
-#include <dune/common/fixedarray.hh>
+#include <dune/common/array.hh>
 #include "static_assert.hh"
 #include <cstddef>
 #include <fstream>

commit 29f61b898379e5e3b7b4574404316b70611135b2

fix backwards compability to dune-common < 2.0 broken in 96aa28c4e3ae13e07234834e8bf5bb41480f6e6e

stuff/misc.hh
@@ -9,7 +9,18 @@
 #include <map>
 #include <assert.h>
 #include <algorithm>
-#include <dune/common/array.hh>
+#ifdef HAVE_CMAKE_CONFIG
+	#include "cmake_config.h"
+#else
+	#include "config.h"
+#endif
+#include <dune/common/version.hh>
+#if DUNE_VERSION_NEWER_REV(DUNE_COMMON,1,3,0) //ie >= 2.0
+	#include <dune/common/array.hh>
+#else
+	#include <dune/common/fixedarray.hh>
+#endif
+
 #include "static_assert.hh"
 #include <cstddef>
 #include <fstream>