wwu-numerik/dune-stuff

commit 05bb19ca7f62710d70b2f70888ab1a45b05cf626

Fixed a bug (boost::fs::path returns a boost::path, not a string)

stuff/filesystem.hh
@@ -20,7 +20,7 @@ std::string pathOnly ( std::string path )
 //! return everything after the last slash
 std::string filenameOnly( const std::string& path )
 {
-	return boost::filesystem::path( path ).filename();
+  return boost::filesystem::path( path ).filename().string();
 }
 
 //! may include filename, will be stripped

commit 49d7dc0f352fa3c53fa1bee9f91d8b38320a9b9a

fix backwards compability to boost-filesystem v2 api broken in 05bb19ca7f62710d70b2f70888ab1a45b05cf626

stuff/filesystem.hh
@@ -20,7 +20,11 @@ std::string pathOnly ( std::string path )
 //! return everything after the last slash
 std::string filenameOnly( const std::string& path )
 {
-  return boost::filesystem::path( path ).filename().string();
+#if BOOST_FILESYSTEM_VERSION > 2
+	return boost::filesystem::path( path ).filename().string();
+#else
+	return boost::filesystem::path( path ).filename();
+#endif
 }
 
 //! may include filename, will be stripped