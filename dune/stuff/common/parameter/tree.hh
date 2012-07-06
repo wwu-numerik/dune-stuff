4829ed3c452b3a0f2b4b0dd187d1c9074dc6de19
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-06 12:11:35 +0200

    added files from dune-helper-tools

---
+++ dune/stuff/common/parameter/tree.hh
@@ -0,0 +1,71 @@
+#ifndef DUNE_HELPER_TOOLS_COMMON_PARAMETERTREE_HH
+#define DUNE_HELPER_TOOLS_COMMON_PARAMETERTREE_HH
+
+// system
+#include <sstream>
+
+// dune-common
+#include <dune/common/exceptions.hh>
+#include <dune/common/parametertree.hh>
+#include <dune/common/parametertreeparser.hh>
+
+namespace Dune
+{
+
+namespace HelperTools
+{
+
+namespace Common
+{
+
+namespace ParameterTree
+{
+
+/**
+  \brief      Fills a Dune::ParameterTree given a parameter file or command line arguments.
+  \param[in]  argc
+              From \c main()
+  \param[in]  argv
+              From \c main()
+  \param[out] paramTree
+              The Dune::ParameterTree that is to be filled.
+  **/
+Dune::ParameterTree init(int argc, char** argv, std::string filename)
+{
+  Dune::ParameterTree paramTree;
+  if (argc == 1) {
+    Dune::ParameterTreeParser::readINITree(filename, paramTree);
+  } else if (argc == 2) {
+    Dune::ParameterTreeParser::readINITree(argv[1], paramTree);
+  } else {
+    Dune::ParameterTreeParser::readOptions(argc, argv, paramTree);
+  }
+  if (paramTree.hasKey("paramfile")) {
+    Dune::ParameterTreeParser::readINITree(paramTree.get< std::string >("paramfile"), paramTree, false);
+  }
+  return paramTree;
+} // Dune::ParameterTree init(int argc, char** argv, std::string filename)
+
+void assertSub(const Dune::ParameterTree& paramTree, std::string sub, std::string id = "")
+{
+  if (!paramTree.hasSub(sub)) {
+    std::stringstream msg;
+    msg << "Error";
+    if (id != "") {
+      msg << " in " << id;
+    }
+    msg << ": subTree '" << sub << "' not found in the following Dune::Parametertree" << std::endl;
+    paramTree.report(msg);
+    DUNE_THROW(Dune::InvalidStateException, msg.str());
+  }
+} // void assert_sub(std::string "sub")
+
+} // namespace ParameterTree
+
+} // namespace Common
+
+} // namespace HelperTools
+
+} // namespace Dune
+
+#endif // DUNE_HELPER_TOOLS_COMMON_PARAMETERTREE_HH
---
+++ dune/stuff/grid/provider/cornerpoint.cc
@@ -0,0 +1,90 @@
+const std::string Cornerpoint::id = "helper-tools.grid.provider.cornerpoint";
+
+Cornerpoint::Cornerpoint(Dune::ParameterTree& paramTree)
+  : grid_()
+{
+  // get correct parameters
+  Dune::ParameterTree* paramsP = &paramTree;
+  if (paramsP->hasSub(id))
+    paramsP = &(paramsP->sub(id));
+  Dune::ParameterTree& params = *paramsP;
+  // build grid
+  const std::string key = "filename";
+  assert(params.hasKey(key));
+  buildGrid(params.get(key, ""));
+  /*buildMDGrid();*/
+} // Cornerpoint::Cornerpoint(const Dune::ParameterTree& paramTree)
+
+Cornerpoint::Cornerpoint(std::string filename)
+  : grid_()
+{
+  buildGrid(filename);
+  /*buildMDGrid();*/
+} // Cornerpoint::Cornerpoint(std::string filename)
+
+Dune::CpGrid& Cornerpoint::grid()
+{
+  return grid_;
+}
+
+const Dune::CpGrid& Cornerpoint::grid() const
+{
+  return grid_;
+}
+
+void Cornerpoint::visualize(Dune::ParameterTree& paramTree)
+{
+  const std::string localId = "visualize";
+  // get correct parameters
+  Dune::ParameterTree* paramsP = &paramTree;
+  if (paramsP->hasSub(id))
+    paramsP = &(paramsP->sub(id));
+  if (paramsP->hasSub(localId))
+    paramsP = &(paramsP->sub(localId));
+  Dune::ParameterTree& params = *paramsP;
+  // check for grid visualization
+  if (params.hasKey("grid")) {
+    const std::string filenameGrid = params.get("grid", id + ".grid");
+    // grid view
+    typedef Dune::CpGrid GridType;
+    GridType& grid = this->grid();
+    typedef GridType::LeafGridView GridView;
+    GridView gridView = grid.leafView();
+    // mapper
+    Dune::LeafMultipleCodimMultipleGeomTypeMapper< GridType, P0Layout > mapper(grid);
+    std::vector< double > data(mapper.size());
+    // walk the grid
+    typedef GridView::Codim<0>::Iterator ElementIterator;
+    typedef GridView::Codim<0>::Entity ElementType;
+    typedef ElementType::LeafIntersectionIterator FacetIteratorType;
+    for (ElementIterator it = gridView.begin<0>(); it != gridView.end<0>(); ++it)
+    {
+      ElementType& element = *it;
+      data[mapper.map(element)] = 0.0;
+      int numberOfBoundarySegments = 0;
+      bool isOnBoundary = false;
+      for (FacetIteratorType facet = element.ileafbegin();
+           facet != element.ileafend();
+           ++facet) {
+        if (!facet->neighbor() && facet->boundary()){
+          isOnBoundary = true;
+          numberOfBoundarySegments += 1;
+          data[mapper.map(element)] += double(facet->boundaryId());
+        }
+      }
+      if (isOnBoundary) {
+        data[mapper.map(element)] /= double(numberOfBoundarySegments);
+      }
+    }
+    // write to vtk
+    Dune::VTKWriter< GridView > vtkwriter(gridView);
+    vtkwriter.addCellData(data, "boundaryId");
+    vtkwriter.write(filenameGrid, Dune::VTK::ascii);
+  } // check for grid visualization
+} // void Cornerpoint::visualize(Dune::ParameterTree& paramTree)
+
+void Cornerpoint::buildGrid(std::string filename)
+{
+  grid_.readEclipseFormat(filename, 0.0, false, false);
+  return;
+} // void Cornerpoint::buildGrid(std::string filename)
