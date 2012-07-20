529cf7f2489fa8c65a5456e05764631096110575
Rene Milk <rene.milk@uni-muenster.de>
2012-07-20 16:23:19 +0200

    misc. depr fixes

--- dune/stuff/common/debug.hh
+++ dune/stuff/common/debug.hh
@@ -84,11 +84,7 @@ public:
  #define ASSERT_EXCEPTION(cond, msg)
 #endif // ifndef NDEBUG
 
-#if 1 /*  there should be no more any compilers needing the "#else" version */
- #define UNUSED(identifier)  /* identifier */
-#else  /*  stupid, broken compiler */
- #define UNUSED(identifier) identifier
-#endif // if 1
+#define UNUSED(identifier)  /* identifier */
 
 /*  some arguments are only used in debug mode, but unused in release one */
 #ifndef NDEBUG
