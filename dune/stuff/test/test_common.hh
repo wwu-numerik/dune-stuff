eb4ba36bc3df987e125cb61d686d4c196cbb000e
Rene Milk <rene.milk@uni-muenster.de>
2012-07-25 13:59:32 +0200

    [tests] tmp type lists make for slicker code

---
+++ dune/stuff/test/test_common.hh
@@ -0,0 +1,57 @@
+#ifndef DUNE_STUFF_TEST_TOOLS_HH
+#define DUNE_STUFF_TEST_TOOLS_HH
+
+#ifdef HAVE_CMAKE_CONFIG
+ #include "cmake_config.h"
+#elif defined (HAVE_CONFIG_H)
+ #include <config.h>
+#endif // ifdef HAVE_CMAKE_CONFIG
+
+#include <dune/common/float_cmp.hh>
+#include <dune/common/fvector.hh>
+#include <dune/common/fmatrix.hh>
+#include <dune/common/tuples.hh>
+#include <dune/common/tupleutility.hh>
+#include <dune/common/mpihelper.hh>
+
+#include <random>
+
+#define MY_ASSERT(cond) \
+    if (! (cond)) { DUNE_THROW(Dune::Exception, #cond  " failed"  ); }
+
+template < typename, bool >
+struct Distribution {
+};
+template < typename T >
+struct Distribution<T,true> {
+    typedef std::uniform_int_distribution <T>
+        type;
+};
+template < typename T >
+struct Distribution<T,false> {
+    typedef std::uniform_real_distribution<T>
+        type;
+};
+
+template < template <class> class Test >
+struct TestRunner {
+    struct Visitor {
+        template <class T>
+        void visit(const T&) {
+            Test<T>().run();
+        }
+    };
+
+    template < class Tuple >
+    static void run() {
+        Tuple t;
+        Dune::ForEachValue<Tuple> fe(t);
+        Visitor v;
+        fe.apply(v);
+    }
+};
+
+typedef Dune::tuple<double, float, //Dune::bigunsignedint,
+  int, unsigned int, unsigned long, long long, char> BasicTypes;
+
+#endif // DUNE_STUFF_TEST_TOOLS_HH
