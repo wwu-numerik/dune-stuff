8b342517a6dc74835ab51f90623a029f5516b60f
Rene Milk <rene.milk@uni-muenster.de>
2012-08-08 17:58:31 +0200

    [common.parameter] introduce mechanism to record gets and default mismatch detection
    (see test/common_paramater_configcontainer.cc for usage)

--- dune/stuff/common/parameter/validation.hh
+++ dune/stuff/common/parameter/validation.hh
@@ -7,13 +7,11 @@
 
 #include <dune/stuff/common/misc.hh>
 #include <dune/stuff/common/string.hh>
+#include <dune/stuff/common/type_utils.hh>
 
 namespace Dune {
-
 namespace Stuff {
-
 namespace Common {
-
 namespace Parameter {
 
 /** \brief Base class for all Validators
@@ -185,11 +183,8 @@ private:
 };
 
 } //end namesapce Parameter
-
 } //end namesapce Common
-
 } //end namesapce Stuff
-
 } //end namesapce Dune
 
 template < class T, class Validator >
