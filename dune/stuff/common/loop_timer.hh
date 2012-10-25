612620509383056c83b4ff81c48d41a31c33ebc3
Rene Milk <rene.milk@uni-muenster.de>
2012-10-25 16:18:28 +0200

    [common] makes loop_timer independent from dune-fem and more precise

--- dune/stuff/common/loop_timer.hh
+++ dune/stuff/common/loop_timer.hh
@@ -2,10 +2,9 @@
 #define DUNE_STUFF_LOOP_TIMER_HH
 
 #include "math.hh"
+#include "profiler.hh"
 
 #include <dune/common/exceptions.hh>
-#include <dune/fem/misc/femtimer.hh>
-
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 namespace Dune {
@@ -44,7 +43,6 @@ struct ProgressiveWeights
 };
 
 /** \brief helper class to estimate time needed to complete a loop with given counter
-  *
   **/
 template< class CounterType, class WeightType = IdentityWeights >
 class LoopTimer
@@ -56,8 +54,8 @@ class LoopTimer
   int iteration_;
   std::ostream& output_stream_;
   WeightType weight_;
-  Dune::Stuff::Common::MovingAverage avg_time_per_iteration_;
-  Dune::ExecutionTimer step_timer_;
+  MovingAverage avg_time_per_iteration_;
+  TimingData step_timer_;
 
 public:
   LoopTimer( CounterType& counter, const int iteration_count, std::ostream& output_stream = std::cout,
@@ -67,23 +65,23 @@ public:
       , iteration_(0)
       , output_stream_(output_stream)
       , weight_(weight)
-  { step_timer_.start(); }
+  {}
 
   ThisType& operator++() {
     ++iteration_;
-    step_timer_.end();
-    avg_time_per_iteration_ += weight_.apply(std::abs( step_timer_.read() ), iteration_, iteration_count_);
+    step_timer_.stop();
+    avg_time_per_iteration_ += weight_.apply(std::abs( step_timer_.delta() ), iteration_, iteration_count_);
     const long remaining_steps = iteration_count_ - iteration_;
-    double remaining_seconds = remaining_steps * double(avg_time_per_iteration_);
-    boost::posix_time::time_duration diff(0, 0, remaining_seconds, 0);
+    const double remaining_ms = remaining_steps * double(avg_time_per_iteration_);
+    boost::posix_time::time_duration diff = boost::posix_time::microseconds(remaining_ms);
     boost::posix_time::ptime target = boost::posix_time::second_clock::local_time();
     target += diff;
     output_stream_ << boost::format("\n---\n Total Time remaining: %s -- %s (%f %%)\n---\n")
-    % boost::posix_time::to_simple_string(diff)
-    % boost::posix_time::to_simple_string(target)
-    % ( 100 * ( remaining_steps / double(iteration_count_) ) )
-    << std::endl;
-    step_timer_.start();
+                            % boost::posix_time::to_simple_string(diff)
+                            % boost::posix_time::to_simple_string(target)
+                            % ( 100 * ( remaining_steps / double(iteration_count_) ) )
+                   << std::endl;
+    step_timer_ = TimingData();
     ++counter_;
     return *this;
   } // ++
