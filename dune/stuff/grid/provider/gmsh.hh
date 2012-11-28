dc21a1b8156b9c4b7abb48a995d92b1cb7fe06d4
Felix Albrecht <mail@felixalbrecht.de>
2012-11-28 14:26:28 +0100

    [grid.provider.gmsh] some cleanup

--- dune/stuff/grid/provider/gmsh.hh
+++ dune/stuff/grid/provider/gmsh.hh
@@ -29,6 +29,9 @@
 //#include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/grid/io/file/gmshreader.hh>
 
+// dune-stuff
+#include <dune/stuff/common/parameter/tree.hh>
+
 // local
 #include "interface.hh"
 
@@ -46,22 +49,27 @@ template< class GridImp = Dune::GridSelector::GridType >
 template< class GridImp = Dune::SGrid< 2, 2 > >
 #endif // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
 class Gmsh
-        : public Interface<GridImp>
+  : public Interface< GridImp >
 {
 public:
   //! Type of the provided grid.
   typedef GridImp GridType;
 
+  typedef Interface< GridType > BaseType;
+
+  typedef Gmsh< GridType > ThisType;
+
   //! Dimension of the provided grid.
-  static const unsigned int dim = GridType::dimension;
+  static const unsigned int dim = BaseType::dim;
 
   //! Type of the grids coordinates.
-  typedef Dune::FieldVector< typename GridType::ctype, dim > CoordinateType;
+  typedef typename BaseType::CoordinateType CoordinateType;
 
-  Gmsh(const Dune::ParameterTree paramTree)
+  Gmsh(const Dune::Stuff::Common::ExtendedParameterTree& paramTree)
   {
     dune_static_assert(!(Dune::is_same<GridType, Dune::YaspGrid< dim > >::value), "GmshReader doesn't work with YaspGrid");
     dune_static_assert(!(Dune::is_same<GridType, Dune::SGrid< 2, 2 > >::value), "GmshReader doesn't work with SGrid");
+    paramTree.assert("mshfile");
     const std::string filename = paramTree.get("mshfile", "sample.msh");
     //read gmshfile
     grid_ = Dune::shared_ptr<GridType> (GmshReader<GridType>::read(filename));
@@ -70,7 +78,7 @@ public:
   //! Unique identifier: \c stuff.grid.provider.gmsh
   static const std::string id()
   {
-    return "stuff.grid.provider.gmsh";
+    return BaseType::id() + ".gmsh";
   }
 
   /**
