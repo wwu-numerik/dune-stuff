c0cd432f8ba9bd4d340eac1994d77834bd0adb4d
Felix Albrecht <mail@felixalbrecht.de>
2012-12-03 12:19:27 +0100

    [function.expression] reenabling parts of revert 2e08a7654458ab8c51132a5414206e11369c2223

--- dune/stuff/function/expression.hh
+++ dune/stuff/function/expression.hh
@@ -175,11 +175,14 @@ public:
   }
 
 #ifdef HAVE_EIGEN
+  /**
+   * \attention ret is resized to size dimRange()!
+   */
   void evaluate(const Eigen::VectorXd& arg, Eigen::VectorXd& ret) const
   {
     // ensure right dimensions
     assert(arg.size() <= maxDimDomain);
-    assert(ret.size() <= dimRange());
+    ret.resize(dimRange());
     // arg
     for (int i = 0; i < arg.size(); ++ i) {
       *(arg_[i]) = arg(i);
@@ -188,7 +191,7 @@ public:
     for (int i = 0; i < ret.size(); ++ i) {
       ret(i) = op_[i]->Val();
     }
-  }
+  } // void evaluate(const Eigen::VectorXd& arg, Eigen::VectorXd& ret) const
 #endif // HAVE_EIGEN
 
 private:
