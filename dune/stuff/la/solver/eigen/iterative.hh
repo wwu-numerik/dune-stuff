5716d31b5686a47bba528ec0538c5b54183ac4ba
Rene Milk <rene.milk@uni-muenster.de>
2012-12-19 14:56:15 +0100

    [la] remove duplicate Bicgstab implementations

--- dune/stuff/la/solver/eigen/iterative.hh
+++ dune/stuff/la/solver/eigen/iterative.hh
@@ -25,13 +25,14 @@ namespace Solver {
 namespace Eigen {
 
 
-template< class MatrixImp, class VectorImp >
-class BicgstabDiagonal;
+template< class MatrixImp, class VectorImp, template < class ElementImp > class Preconditioner >
+class Bicgstab;
 
 
-template< class ElementImp >
-class BicgstabDiagonal< Container::Eigen::RowMajorSparseMatrix< ElementImp >,
-                        Container::Eigen::DenseVector< ElementImp > >
+template< class ElementImp, template < class ElementImp > class Preconditioner >
+class Bicgstab< Container::Eigen::RowMajorSparseMatrix< ElementImp >,
+                Container::Eigen::DenseVector< ElementImp >,
+                Preconditioner>
   : public Interface< Container::Eigen::RowMajorSparseMatrix< ElementImp >,
                       Container::Eigen::DenseVector< ElementImp > >
 {
@@ -49,12 +50,12 @@ public:
   typedef typename BaseType::size_type size_type;
 
 private:
-  typedef ::Eigen::DiagonalPreconditioner< ElementType > PreconditionerType;
+  typedef Preconditioner< ElementType > PreconditionerType;
 
   typedef ::Eigen::BiCGSTAB< typename MatrixType::BaseType, PreconditionerType > SolverType;
 
 public:
-  BicgstabDiagonal()
+  Bicgstab()
     : initialized_(false)
   {}
 
@@ -93,58 +94,21 @@ class BicgstabIncompleteLUT;
 template< class ElementImp >
 class BicgstabIncompleteLUT<  Container::Eigen::RowMajorSparseMatrix< ElementImp >,
                               Container::Eigen::DenseVector< ElementImp > >
-  : public Interface< Container::Eigen::RowMajorSparseMatrix< ElementImp >,
-                      Container::Eigen::DenseVector< ElementImp > >
-{
-public:
-  typedef Interface<  Container::Eigen::RowMajorSparseMatrix< ElementImp >,
-                      Container::Eigen::DenseVector< ElementImp > >
-      BaseType;
+  : public Bicgstab<  Container::Eigen::RowMajorSparseMatrix< ElementImp >,
+                      Container::Eigen::DenseVector< ElementImp >,
+                      ::Eigen::IncompleteLUT>
+{};
 
-  typedef typename BaseType::MatrixType MatrixType;
+template< class MatrixImp, class VectorImp >
+class BicgstabDiagonal;
 
-  typedef typename BaseType::VectorType VectorType;
-
-  typedef typename BaseType::ElementType ElementType;
-
-  typedef typename BaseType::size_type size_type;
-
-private:
-  typedef ::Eigen::IncompleteLUT< ElementType > PreconditionerType;
-
-  typedef ::Eigen::BiCGSTAB< typename MatrixType::BaseType, PreconditionerType > SolverType;
-
-public:
-  BicgstabIncompleteLUT()
-    : initialized_(false)
-  {}
-
-  virtual void init(const MatrixType& systemMatrix)
-  {
-    if (initialized_)
-      DUNE_THROW(Dune::InvalidStateException, "\nERROR: init() may only be called once!");
-    solver_ = new SolverType(systemMatrix.base());
-    initialized_ = true;
-  } // virtual void init(...)
-
-  virtual bool apply(const VectorType& rhsVector,
-                     VectorType& solutionVector,
-                     const size_type maxIter = 5000,
-                     const ElementType precision = 1e-12) const
-  {
-    if (!initialized_)
-      DUNE_THROW(Dune::InvalidStateException, "\nERROR: please call init() before calling apply()!");
-    solver_->setMaxIterations(maxIter);
-    solver_->setTolerance(precision);
-    solutionVector.base() = solver_->solve(rhsVector.base());
-    const ::Eigen::ComputationInfo info = solver_->info();
-    return (info == ::Eigen::Success);
-  } // virtual bool apply(...)
-
-private:
-  bool initialized_;
-  SolverType* solver_;
-}; // class BicgstabIncompleteLUT
+template< class ElementImp >
+class BicgstabDiagonal< Container::Eigen::RowMajorSparseMatrix< ElementImp >,
+                        Container::Eigen::DenseVector< ElementImp > >
+  : public Bicgstab<  Container::Eigen::RowMajorSparseMatrix< ElementImp >,
+                      Container::Eigen::DenseVector< ElementImp >,
+                      ::Eigen::DiagonalPreconditioner>
+{};
 
 
 template< class MatrixImp, class VectorImp >
