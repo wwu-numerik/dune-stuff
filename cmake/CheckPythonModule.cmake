9c4f13a5fae475cd5d023c1d7cebea1f88fe6850
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-21 16:25:58 +0100

    [cmake] added pybindgen

---
+++ cmake/CheckPythonModule.cmake
@@ -0,0 +1,15 @@
+# check whether given python module exists
+
+macro(CheckPythonModule module)
+	execute_process(
+		COMMAND
+			${PYTHON_EXECUTABLE} "${PROJECT_SOURCE_DIR}/../dune-stuff/cmake/CheckPythonModule.py" "${module}"
+		WORKING_DIRECTORY
+			${CMAKE_CURRENT_SOURCE_DIR}
+		RESULT_VARIABLE
+			python_${module}_RESULT_VAR
+		)
+	IF( python_${module}_RESULT_VAR EQUAL 0 )
+		set( python_${module}_FOUND 1 )
+	ENDIF( python_${module}_RESULT_VAR EQUAL 0 )
+endmacro(CheckPythonModule)
