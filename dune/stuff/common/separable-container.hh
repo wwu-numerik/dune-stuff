9fcc897ac3f60e50aa2c0c47e9f31236fb0be30f
Felix Albrecht <mail@felixalbrecht.de>
2013-01-28 21:25:13 +0100

    [common.separable-container] added

---
+++ dune/stuff/common/separable-container.hh
@@ -0,0 +1,76 @@
+#ifndef DUNE_STUFF_COMMON_SEPARABLE_CONTAINER_HH
+#define DUNE_STUFF_COMMON_SEPARABLE_CONTAINER_HH
+
+#include <vector>
+
+#include <dune/common/exceptions.hh>
+#include <dune/common/shared_ptr.hh>
+
+namespace Dune {
+namespace Stuff {
+namespace Common {
+
+
+template< class ComponentImp, class CoefficientImp, class size_t = unsigned int >
+class SeparableContainer
+{
+public:
+  typedef ComponentImp ComponentType;
+
+  typedef CoefficientImp CoefficientType;
+
+  typedef size_t size_type;
+
+  SeparableContainer(const size_type _paramSize,
+                     const std::vector< Dune::shared_ptr< const ComponentType > > _components,
+                     const std::vector< Dune::shared_ptr< const CoefficientType > > _coefficients)
+    : paramSize_(_paramSize)
+    , components_(_components)
+    , coefficients_(_coefficients)
+  {
+    assert(components_.size() == coefficients_.size() || components_.size() == coefficients_.size() + 1);
+  }
+
+  SeparableContainer(const size_type _paramSize, const Dune::shared_ptr< const ComponentType > _component)
+    : paramSize_(_paramSize)
+  {
+    components_.push_back(_component);
+  }
+
+  const size_type paramSize() const
+  {
+    return paramSize_;
+  }
+
+  const size_type numComponents() const
+  {
+    return components_.size();
+  }
+
+  const std::vector< Dune::shared_ptr< const ComponentType > >& components() const
+  {
+    return components_;
+  }
+
+  const size_type numCoefficients() const
+  {
+    return coefficients_.size();
+  }
+
+  const std::vector< Dune::shared_ptr< const CoefficientType > >& coefficients() const
+  {
+    return coefficients_;
+  }
+
+private:
+  const size_type paramSize_;
+  std::vector< Dune::shared_ptr< const ComponentType > > components_;
+  const std::vector< Dune::shared_ptr< const CoefficientType > > coefficients_;
+}; // class SeparableContainer
+
+
+} // namespace Common
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_STUFF_COMMON_SEPARABLE_CONTAINER_HH
