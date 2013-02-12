d93e307f98dfca0966f241739e5bca3a3beffa05
Rene Milk <rene.milk@uni-muenster.de>
2013-02-12 16:56:31 +0100

    [grid] Provider::createFromDescription return pointers now

--- dune/stuff/grid/provider/gmsh.hh
+++ dune/stuff/grid/provider/gmsh.hh
@@ -93,7 +93,7 @@ public:
     }
   }
 
-  static ThisType createFromDescription(const Dune::ParameterTree& paramTree, const std::string subName = id())
+  static ThisType* createFromDescription(const Dune::ParameterTree& paramTree, const std::string subName = id())
   {
     // get correct paramTree
     Dune::Stuff::Common::ExtendedParameterTree extendedParamTree;
@@ -106,7 +106,7 @@ public:
       DUNE_THROW(Dune::RangeError,
                  "\nMissing key 'filename' in the following Dune::ParameterTree:\n" << extendedParamTree.reportString("  "));
     const std::string filename = extendedParamTree.get("filename", "meaningless_default_value");
-    return Gmsh(filename);
+    return new ThisType(filename);
   }
 
   ThisType& operator=(ThisType& other)
