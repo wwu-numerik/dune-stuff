6b7d606ca283aeb6b8ff52ebb642361caa2149f8
Rene Milk <rene.milk@uni-muenster.de>
2013-02-13 12:06:42 +0100

    [common.string] new deprecation marks

--- dune/stuff/common/string.hh
+++ dune/stuff/common/string.hh
@@ -141,11 +141,13 @@ DUNE_DEPRECATED_MSG("use DSC::toString instead, removal with stuff 2.3") std::st
   return s;
 }
 
+DUNE_DEPRECATED_MSG("nonsense function, to be removed immenently")
 bool equal(const std::string& first, const std::string& second)
 {
   return !first.compare(second);
 }
 
+DUNE_DEPRECATED_MSG("use the constructor call directly")
 std::vector< std::string > mainArgsToVector(int argc, char** argv)
 {
   return std::vector< std::string >(argv, argv + argc);
