eae8884da58da35bafb786cc4a5a2072611b08c9
Felix Albrecht <mail@felixalbrecht.de>
2013-03-05 03:25:04 +0100

    [function.product] added, not tested yet

---
+++ dune/stuff/function/product.hh
@@ -0,0 +1,238 @@
+#ifndef DUNE_STUFF_FUNCTION_PRODUCT_HH
+#define DUNE_STUFF_FUNCTION_PRODUCT_HH
+
+#include <dune/common/exceptions.hh>
+#include <dune/common/shared_ptr.hh>
+
+#include <dune/stuff/common/color.hh>
+
+#include "interface.hh"
+
+namespace Dune {
+namespace Stuff {
+namespace Function {
+
+
+// forward, to allow for specialization
+template< class LeftFactorType, class RightFactorType >
+class Product;
+
+
+template< class DomainFieldImp, int domainDim, class RangeFieldImp >
+class Product<  Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >,
+                Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 > >
+  : Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >
+{
+public:
+  typedef Product<  Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >,
+                    Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 > >  ThisType;
+  typedef Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >              BaseType;
+
+  typedef Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >              LeftFactorType;
+  typedef Interface< DomainFieldImp, domainDim, RangeFieldImp, 1 >              RightFactorType;
+
+  typedef typename BaseType::DomainFieldType  DomainFieldType;
+  static const int                            dimDomain = BaseType::dimDomain;
+  typedef typename BaseType::DomainType       DomainType;
+
+  typedef typename BaseType::RangeFieldType   RangeFieldType;
+  static const int                            dimRange = BaseType::dimRange;
+  typedef typename BaseType::RangeType        RangeType;
+
+  typedef typename BaseType::ParamFieldType ParamFieldType;
+  static const int                          maxParamDim = BaseType::maxParamDim;
+  typedef typename BaseType::ParamType      ParamType;
+
+  typedef typename BaseType::ComponentType    ComponentType;
+  typedef typename BaseType::CoefficientType  CoefficientType;
+
+  static std::string id()
+  {
+    return BaseType::id() + ".product";
+  }
+
+  Product(const Dune::shared_ptr< const LeftFactorType > _leftFactor,
+          const Dune::shared_ptr< const RightFactorType > _rightFactor)
+    : leftFactor_(_leftFactor)
+    , rightFactor_(_rightFactor)
+    , leftParametric_(leftFactor_->parametric())
+    , rightParametric_(rightFactor_->parametric())
+    , leftSeparable_(leftParametric_ ? leftFactor_->separable() : false)
+    , rightSeparable_(rightParametric_ ? rightFactor_->separable() : false)
+    , name_("product of '" + leftFactor_->name() + "' and '" + rightFactor_->name())
+    , order_((leftFactor_->order() >= 0 && rightFactor_->order() >= 0)
+             ? (leftFactor_->order() + rightFactor_->order())
+             : -1)
+  {
+    if (leftParametric_ && rightParametric_)
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " only one factor may be parametric!");
+  } // Product
+
+  Dune::shared_ptr< const LeftFactorType >  leftFactor() const
+  {
+    return leftFactor_;
+  }
+
+  Dune::shared_ptr< const RightFactorType > rightFactor() const
+  {
+    return rightFactor_;
+  }
+
+  virtual bool parametric() const
+  {
+    return leftParametric_ || rightParametric_;
+  }
+
+  virtual std::string name() const
+  {
+    return name_;
+  }
+
+  virtual int order() const
+  {
+    return order_;
+  }
+
+  virtual void evaluate(const DomainType& x, RangeType& ret) const
+  {
+    if (parametric())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " nonparametric evaluate() called for a parametric function!");
+    else {
+      leftFactor_->evaluate(x, ret);
+      RangeType tmp(0);
+      rightFactor_->evaluate(x, tmp);
+      ret[0] *= tmp[0];
+    }
+  } // ... evaluate(...)
+
+  virtual void evaluate(const DomainType& x, const ParamType& mu, RangeType& ret) const
+  {
+    if (!parametric())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " parametric evaluate() called for a nonparametric function!");
+    RangeType leftValue(0);
+    RangeType rightValue(0);
+    if (leftParametric_)
+      leftFactor_->evaluate(x, mu, leftValue);
+    else
+      leftFactor_->evaluate(x, leftValue);
+    if (rightParametric_)
+      rightFactor_->evaluate(x, mu, rightValue);
+    else
+      rightFactor_->evaluate(x, rightValue);
+    ret[0] = leftValue[0];
+    ret[0] *= rightValue[0];
+  } // ... evaluate(...)
+
+  virtual size_t paramSize() const
+  {
+    if (leftParametric_)
+      return leftFactor_->paramSize();
+    else if (rightParametric_)
+      return rightFactor_->paramSize();
+    else
+      return 0;
+  } // ... paramSize(...)
+
+  virtual const std::vector< ParamType >& paramRange() const
+  {
+    if (!parametric())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " paramRange() called for a nonparametric function!");
+    if (leftParametric_)
+      return leftFactor_->paramRange();
+    else
+      return rightFactor_->paramRange();
+  } // ... paramRange(...)
+
+  virtual const std::vector< std::string >& paramExplanation() const
+  {
+    if (!parametric())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " paramExplanation() called for a nonparametric function!");
+    if (leftParametric_)
+      return leftFactor_->paramExplanation();
+    else
+      return rightFactor_->paramExplanation();
+  } // ... paramExplanation(...)
+
+  virtual bool separable() const
+  {
+    if (!parametric())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " separable() called for a nonparametric function!");
+    return leftSeparable_ || rightSeparable_;
+  } // ... separable(...)
+
+  virtual size_t numComponents() const
+  {
+    if (!separable())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " numComponents() called for a nonseparable function!");
+    if (leftSeparable_)
+      return leftFactor_->numComponents();
+    else
+      return rightFactor_->numComponents();
+  } // ... numComponents(...)
+
+  virtual const std::vector< Dune::shared_ptr< const ComponentType > >& components() const
+  {
+    if (!separable())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " components() called for a nonseparable function!");
+    if (leftSeparable_)
+      return leftFactor_->components();
+    else
+      return rightFactor_->components();
+  } // ... components(...)
+
+  virtual size_t numCoefficients() const
+  {
+    if (!separable())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " numCoefficients() called for a nonseparable function!");
+    if (leftSeparable_)
+      return leftFactor_->numCoefficients();
+    else
+      return rightFactor_->numCoefficients();
+  } // ... numCoefficients(...)
+
+  virtual const std::vector< Dune::shared_ptr< const CoefficientType > >& coefficients() const
+  {
+    if (!separable())
+      DUNE_THROW(Dune::InvalidStateException,
+                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                 << " coefficients() called for a nonseparable function!");
+    if (leftSeparable_)
+      return leftFactor_->coefficients();
+    else
+      return rightFactor_->coefficients();
+  } // ... coefficients(...)
+
+private:
+  const Dune::shared_ptr< const LeftFactorType >  leftFactor_;
+  const Dune::shared_ptr< const RightFactorType > rightFactor_;
+  const bool leftParametric_;
+  const bool rightParametric_;
+  const bool leftSeparable_;
+  const bool rightSeparable_;
+  const std::string name_;
+  const int order_;
+}; // class Product< ..., ... >
+
+} // namespace Function
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_STUFF_FUNCTION_PRODUCT_HH
