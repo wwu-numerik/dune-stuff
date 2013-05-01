64090ceb007f03bd64474cec74fb400cf1557f82
Felix Albrecht <mail@felixalbrecht.de>
2013-05-01 17:40:54 +0200

    [function.fixed] added FunctionFixedTime

--- dune/stuff/function/fixed.hh
+++ dune/stuff/function/fixed.hh
@@ -74,6 +74,54 @@ private:
 }; // class FunctionFixedParameter
 
 
+template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows, int rangeDimCols >
+class FunctionFixedTime
+  : public FunctionInterface< DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows, rangeDimCols >
+{
+  typedef FunctionInterface< DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows, rangeDimCols > BaseType;
+public:
+  typedef typename BaseType::DomainFieldType  DomainFieldType;
+  static const int                            dimDomain = BaseType::dimDomain;
+  typedef typename BaseType::DomainType       DomainType;
+  typedef typename BaseType::RangeFieldType   RangeFieldType;
+  static const int                            dimRangeRows = BaseType::dimRangeRows;
+  static const int                            dimRangeCols = BaseType::dimRangeCols;
+  typedef typename BaseType::RangeType        RangeType;
+  typedef typename BaseType::ParamType        ParamType;
+
+  typedef TimedependentFunctionInterface< DomainFieldType, dimDomain,
+                                          RangeFieldType, dimRangeRows, dimRangeCols > WrappedType;
+
+  static std::string id()
+  {
+    return BaseType::id() + ".fixed";
+  }
+
+  FunctionFixedTime(const std::shared_ptr< const WrappedType > timedependentFunction,
+                    const double time)
+    : wrappedFunction_(timedependentFunction)
+    , time_(time)
+  {}
+
+  virtual std::string name() const
+  {
+    return "fixed '" + wrappedFunction_->name() + "' for time '" + Dune::Stuff::Common::toString(time_);
+  }
+
+  virtual int order() const
+  {
+    return wrappedFunction_->order();
+  }
+
+  virtual void evaluate(const DomainType& x, RangeType& ret) const
+  {
+    wrappedFunction_->evaluate(x, time_, ret);
+  }
+
+private:
+  const std::shared_ptr< const WrappedType > wrappedFunction_;
+  const double time_;
+}; // class FunctionFixedTime
 
 
 } // namespace Stuff
