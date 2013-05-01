390f59459530815fc264a95b15f1e915090aa094
Felix Albrecht <mail@felixalbrecht.de>
2013-05-01 21:41:14 +0200

    [localfunction.interface] finished so far, refs #213

--- dune/stuff/localfunction/interface.hh
+++ dune/stuff/localfunction/interface.hh
@@ -9,22 +9,27 @@ namespace Dune {
 namespace Stuff {
 
 
-template< class DomainFieldImp, unsigned int domainDim, class RangeFieldImp, unsigned int rangeDim, class Traits >
+/**
+ *  \brief  Interface for matrix valued globalvalued functions, which can be evaluated locally on one Entity.
+ *
+ *          This is the interface for matrixvalued functions, see the specialization for rangeDimCols = 1 for scalar
+ *          and vector valued functions.
+ */
+template< class Traits, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows, int rangeDimCols >
 class LocalFunctionInterface
 {
 public:
-//  typedef LocalFunctionInterface< Traits >  ThisType;
-  typedef typename Traits::derived_type     derived_type;
-
-  typedef typename Traits::EntityType EntityType;
+  typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType   EntityType;
 
   typedef DomainFieldImp                                  DomainFieldType;
   static const unsigned int                               dimDomain = domainDim;
   typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
 
-  typedef RangeFieldImp                                   RangeFieldType;
-  static const unsigned int                               dimRange = rangeDim;
-  typedef Dune::FieldVector< RangeFieldType, dimDomain >  RangeType;
+  typedef RangeFieldImp                                                   RangeFieldType;
+  static const unsigned int                                               dimRangeRows = rangeDimRows;
+  static const unsigned int                                               dimRangeCols = rangeDimCols;
+  typedef Dune::FieldMatrix< RangeFieldType, dimRangeRows, dimRangeCols > RangeType;
 
   const EntityType& entity() const
   {
@@ -55,36 +60,60 @@ public:
 }; // class LocalFunctionInterface
 
 
-//template< class Traits >
-//class LocalDifferentiableFunctionInterface
-//  : public LocalFunctionInterface< Traits >
-//{
-//public:
-//  typedef LocalDifferentiableFunctionInterface< Traits >  ThisType;
-//  typedef LocalFunctionInterface< Traits >                BaseType;
-//  typedef typename Traits::derived_type                   derived_type;
-//  typedef typename Traits::EntityType EntityType;
+/**
+ *  \brief  Interface for scalar and vector valued globalvalued functions, which can be evaluated locally on one Entity.
+ */
+template< class Traits, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim >
+class LocalFunctionInterface< Traits, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >
+{
+public:
+  typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType   EntityType;
 
-//  typedef Traits::DomainFieldType                         DomainFieldType;
-//  static const unsigned int                               dimDomain = Traits::dimDomain;
-//  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
+  typedef DomainFieldImp                                  DomainFieldType;
+  static const unsigned int                               dimDomain = domainDim;
+  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
 
-//  typedef Traits::RangeFieldType                          RangeFieldType;
-//  static const unsigned int                               dimRange = Traits::dimRange;
-//  typedef Dune::FieldVector< RangeFieldType, dimDomain >  RangeType;
+  typedef RangeFieldImp                                 RangeFieldType;
+  static const unsigned int                             dimRange = rangeDim;
+  static const unsigned int                             dimRangeRows = dimRange;
+  static const unsigned int                             dimRangeCols = 1;
+  typedef Dune::FieldVector< RangeFieldType, dimRange > RangeType;
 
-//  typedef Dune::FieldMatrix< RangeFieldType, dimRange, dimDomain > JacobianRangeType;
+  const EntityType& entity() const
+  {
+    CHECK_INTERFACE_IMPLEMENTATION(asImp().entity());
+    return asImp().entity();
+  }
 
-//  using BaseType::entity;
-//  using BaseType::evaluate;
-//  using BaseType::asImp;
+  virtual int order() const
+  {
+    return -1;
+  }
 
-//  void jacobian(const DomainType& x, JacobianRangeType& ret) const
-//  {
-//    CHECK_INTERFACE_IMPLEMENTATION(asImp().jacobian(x, ret));
-//    asImp().jacobian(x, ret);
-//  }
-//}; // class LocalDifferentiableFunctionInterface
+  void evaluate(const DomainType& x, RangeType& ret) const
+  {
+    CHECK_INTERFACE_IMPLEMENTATION(asImp().evaluate(x, ret));
+    asImp().evaluate(x, ret);
+  }
+
+  derived_type& asImp()
+  {
+    return static_cast< derived_type& >(*this);
+  }
+
+  const derived_type& asImp() const
+  {
+    return static_cast< const derived_type& >(*this);
+  }
+}; // class LocalFunctionInterface< ..., 1 >
+
+
+/**
+ *  \brief  Flag for all functions which provide a localFunction(entity) method.
+ */
+class LocalizableFunction
+{};
 
 
 } // namespace Stuff
