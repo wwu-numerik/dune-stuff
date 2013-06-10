3e01161386c61aa7391ee4c17b105d494c14724d
Felix Albrecht <mail@felixalbrecht.de>
2013-06-10 18:26:46 +0200

    [function.interface]

--- dune/stuff/function/interface.hh
+++ dune/stuff/function/interface.hh
@@ -431,6 +431,7 @@ public:
   virtual void evaluate(const DomainType& /*_x*/, RangeType& /*_ret*/) const = 0;
   /* @} */
 
+  using BaseType::evaluate;
   using BaseType::localFunction;
 }; // class FunctionInterface
 
