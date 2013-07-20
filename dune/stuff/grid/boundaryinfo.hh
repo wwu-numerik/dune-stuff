5c942180b46cce8987761d7ad517f048aacf8472
Felix Albrecht <mail@felixalbrecht.de>
2013-07-20 21:34:37 +0200

    enable/disable warnings

--- dune/stuff/grid/boundaryinfo.hh
+++ dune/stuff/grid/boundaryinfo.hh
@@ -205,9 +205,7 @@ public:
   {
     if (hasDirichlet_ && intersection.boundary()) {
       // get boundary id
-#include <dune/stuff/common/header/disable_warnings.hh>
       const IdType boundaryId = intersection.boundaryId();
-#include <dune/stuff/common/header/reenable_warnings.hh>
       // get set of dirichlet ids (has to be found, otherwise hasDirichlet_ would be false)
       const typename IdSetMapType::const_iterator result = boundaryInfoMap_->find("dirichlet");
       assert(result != boundaryInfoMap_->end());
