9727207e72339c27d0c51c4206c6a1cabb76a00e
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2013-08-26 12:26:19 +0200

    Replaced std::abs by direct computation of absolute value
    Necessary because no overloaded version of std::abs
    exists for unsigned long long

--- dune/stuff/common/ranges.hh
+++ dune/stuff/common/ranges.hh
@@ -178,7 +178,11 @@ intersectionRange(const Dune::Fem::GridPartInterface<GridPartTraits>& gridpart,
 //! get a vector with values in [start : increment : end)
 template < class T, class sequence = std::vector<T> >
 sequence valueRange(const T start, const T end, const T increment = Epsilon<T>::value) {
-  sequence ret(typename sequence::size_type(((end>start) ? end-start : start-end)/std::abs(increment)), start);
+  // sadly, no overloaded version of std::abs is available for
+  // unsigned long long, so we compute the absolute value of increment
+  // ourselfs
+  T incrementAbs = (increment >= T() ? increment : -increment);
+  sequence ret(typename sequence::size_type(((end>start) ? end-start : start-end)/incrementAbs), start);
   typename sequence::size_type i = 0;
   std::generate(std::begin(ret), std::end(ret), [&](){ return T(start + (increment * i++)); });
   return ret;
