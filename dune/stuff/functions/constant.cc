14674ee5ae09b94d7ac43780118314b3ffc8b4a7
Felix Albrecht <mail@felixalbrecht.de>
2013-10-18 20:40:14 +0200

    [functions.constant] use generator macros for lib

--- dune/stuff/functions/constant.cc
+++ dune/stuff/functions/constant.cc
@@ -19,14 +19,14 @@ namespace Function {
 // ====================
 // ===== Constant =====
 // ====================
-template< class E, class D, int d, class R, int r >
-std::string Constant< E, D, d, R, r >::static_id()
+template< class E, class D, int d, class R, int r, int rC >
+std::string Constant< E, D, d, R, r, rC >::static_id()
 {
   return BaseType::static_id() + ".constant";
 }
 
-template< class E, class D, int d, class R, int r >
-Dune::ParameterTree Constant< E, D, d, R, r >::defaultSettings(const std::string subName)
+template< class E, class D, int d, class R, int r, int rC >
+Dune::ParameterTree Constant< E, D, d, R, r, rC >::defaultSettings(const std::string subName)
 {
   Dune::ParameterTree description;
   description["value"] = "1.0";
@@ -39,34 +39,34 @@ Dune::ParameterTree Constant< E, D, d, R, r >::defaultSettings(const std::string
   }
 } // ... defaultSettings(...)
 
-template< class E, class D, int d, class R, int r >
-typename Constant< E, D, d, R, r >::ThisType* Constant< E, D, d, R, r >::create(const DSC::ExtendedParameterTree settings)
+template< class E, class D, int d, class R, int r, int rC >
+typename Constant< E, D, d, R, r, rC >::ThisType* Constant< E, D, d, R, r, rC >::create(const DSC::ExtendedParameterTree settings)
 {
-  typedef typename Constant< E, D, d, R, r >::ThisType ThisType;
-  typedef typename Constant< E, D, d, R, r >::RangeFieldType RangeFieldType;
+  typedef typename Constant< E, D, d, R, r, rC >::ThisType ThisType;
+  typedef typename Constant< E, D, d, R, r, rC >::RangeFieldType RangeFieldType;
   return new ThisType(settings.get< RangeFieldType >("value", RangeFieldType(0)));
 } // ... create(...)
 
-template< class E, class D, int d, class R, int r >
-Constant< E, D, d, R, r >::Constant(const RangeFieldType& val, const std::string nm)
+template< class E, class D, int d, class R, int r, int rC >
+Constant< E, D, d, R, r, rC >::Constant(const RangeFieldType& val, const std::string nm)
   : value_(std::make_shared< RangeType >(val))
   , name_(nm)
 {}
 
-template< class E, class D, int d, class R, int r >
-Constant< E, D, d, R, r >::Constant(const RangeType& val, const std::string nm)
+template< class E, class D, int d, class R, int r, int rC >
+Constant< E, D, d, R, r, rC >::Constant(const RangeType& val, const std::string nm)
   : value_(std::make_shared< RangeType >(val))
   , name_(nm)
 {}
 
-template< class E, class D, int d, class R, int r >
-Constant< E, D, d, R, r >::Constant(const ThisType& other)
+template< class E, class D, int d, class R, int r, int rC >
+Constant< E, D, d, R, r, rC >::Constant(const ThisType& other)
   : value_(other.value_)
   , name_(other.name_)
 {}
 
-template< class E, class D, int d, class R, int r >
-typename Constant< E, D, d, R, r >::ThisType& Constant< E, D, d, R, r >::operator=(const ThisType& other)
+template< class E, class D, int d, class R, int r, int rC >
+typename Constant< E, D, d, R, r, rC >::ThisType& Constant< E, D, d, R, r, rC >::operator=(const ThisType& other)
 {
   if (this != &other) {
     value_ = other.value_;
@@ -75,21 +75,21 @@ typename Constant< E, D, d, R, r >::ThisType& Constant< E, D, d, R, r >::operato
   return *this;
 }
 
-template< class E, class D, int d, class R, int r >
-typename Constant< E, D, d, R, r >::ThisType* Constant< E, D, d, R, r >::copy() const
+template< class E, class D, int d, class R, int r, int rC >
+typename Constant< E, D, d, R, r, rC >::ThisType* Constant< E, D, d, R, r, rC >::copy() const
 {
   return new ThisType(*this);
 }
 
-template< class E, class D, int d, class R, int r >
-std::string Constant< E, D, d, R, r >::name() const
+template< class E, class D, int d, class R, int r, int rC >
+std::string Constant< E, D, d, R, r, rC >::name() const
 {
   return name_;
 }
 
-template< class E, class D, int d, class R, int r >
-    std::unique_ptr< typename Constant< E, D, d, R, r >::LocalfunctionType >
-Constant< E, D, d, R, r >::local_function(const EntityType& entity) const
+template< class E, class D, int d, class R, int r, int rC >
+    std::unique_ptr< typename Constant< E, D, d, R, r, rC >::LocalfunctionType >
+Constant< E, D, d, R, r, rC >::local_function(const EntityType& entity) const
 {
   return std::unique_ptr< Localfunction >(new Localfunction(entity, value_));
 }
@@ -99,75 +99,76 @@ Constant< E, D, d, R, r >::local_function(const EntityType& entity) const
 } // namespace Stuff
 } // namespace Dune
 
+#define DSF_LIST_DIMDOMAIN(etype) \
+  DSF_LIST_DIMRANGE(etype, 1) \
+  DSF_LIST_DIMRANGE(etype, 2) \
+  DSF_LIST_DIMRANGE(etype, 3)
+
+#define DSF_LIST_DIMRANGE(etype, ddim) \
+  DSF_LIST_DIMRANGECOLS(Dune::Stuff::Function::Constant, etype, ddim, 1) \
+  DSF_LIST_DIMRANGECOLS(Dune::Stuff::Function::Constant, etype, ddim, 2) \
+  DSF_LIST_DIMRANGECOLS(Dune::Stuff::Function::Constant, etype, ddim, 3)
+
+#define DSF_LIST_DIMRANGECOLS(cname, etype, ddim, rdim) \
+  DSF_LIST_DOMAINFIELDTYPES(cname, etype, ddim, rdim, 1) \
+  DSF_LIST_DOMAINFIELDTYPES(cname, etype, ddim, rdim, 2) \
+  DSF_LIST_DOMAINFIELDTYPES(cname, etype, ddim, rdim, 3)
+
+#define DSF_LIST_DOMAINFIELDTYPES(cname, etype, ddim, rdim, rcdim) \
+  DSF_LIST_RANGEFIELDTYPES(cname, etype, double, ddim, rdim, rcdim)
+
+#define DSF_LIST_RANGEFIELDTYPES(cname, etype, dftype, ddim, rdim, rcdim) \
+  DSF_LAST_EXPANSION(cname, etype, dftype, ddim, double, rdim, rcdim) \
+  DSF_LAST_EXPANSION(cname, etype, dftype, ddim, long double, rdim, rcdim)
+
+#define DSF_LAST_EXPANSION(cname, etype, dftype, ddim, rftype, rdim, rcdim) \
+  template class cname< etype, dftype, ddim, rftype, rdim, rcdim >;
+
 #include <dune/stuff/grid/fakeentity.hh>
 
 typedef Dune::Stuff::Grid::FakeEntity< 1 > DuneStuffFake1dEntityType;
 typedef Dune::Stuff::Grid::FakeEntity< 2 > DuneStuffFake2dEntityType;
 typedef Dune::Stuff::Grid::FakeEntity< 3 > DuneStuffFake3dEntityType;
 
-template class Dune::Stuff::Function::Constant< DuneStuffFake1dEntityType, double, 1, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake1dEntityType, double, 1, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake1dEntityType, double, 1, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake2dEntityType, double, 2, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake2dEntityType, double, 2, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake2dEntityType, double, 2, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake3dEntityType, double, 3, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake3dEntityType, double, 3, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneStuffFake3dEntityType, double, 3, double, 3 >;
+DSF_LIST_DIMDOMAIN(DuneStuffFake1dEntityType)
+DSF_LIST_DIMDOMAIN(DuneStuffFake2dEntityType)
+DSF_LIST_DIMDOMAIN(DuneStuffFake3dEntityType)
 
-//#ifdef HAVE_DUNE_GRID
+#ifdef HAVE_DUNE_GRID
 
-//# include <dune/grid/sgrid.hh>
+# include <dune/grid/sgrid.hh>
 
-//typedef typename Dune::SGrid< 1, 1 >::template Codim< 0 >::Entity DuneSGrid1dEntityType;
-//typedef typename Dune::SGrid< 2, 2 >::template Codim< 0 >::Entity DuneSGrid2dEntityType;
-//typedef typename Dune::SGrid< 3, 3 >::template Codim< 0 >::Entity DuneSGrid3dEntityType;
+typedef typename Dune::SGrid< 1, 1 >::template Codim< 0 >::Entity DuneSGrid1dEntityType;
+typedef typename Dune::SGrid< 2, 2 >::template Codim< 0 >::Entity DuneSGrid2dEntityType;
+typedef typename Dune::SGrid< 3, 3 >::template Codim< 0 >::Entity DuneSGrid3dEntityType;
 
-//template class Dune::Stuff::Function::Constant< DuneSGrid1dEntityType, double, 1, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid1dEntityType, double, 1, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid1dEntityType, double, 1, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid2dEntityType, double, 2, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid2dEntityType, double, 2, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid2dEntityType, double, 2, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid3dEntityType, double, 3, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid3dEntityType, double, 3, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneSGrid3dEntityType, double, 3, double, 3 >;
+DSF_LIST_DIMDOMAIN(DuneSGrid1dEntityType)
+DSF_LIST_DIMDOMAIN(DuneSGrid2dEntityType)
+DSF_LIST_DIMDOMAIN(DuneSGrid3dEntityType)
 
-//# include <dune/grid/yaspgrid.hh>
+# include <dune/grid/yaspgrid.hh>
 
-//typedef typename Dune::YaspGrid< 1 >::template Codim< 0 >::Entity DuneYaspGrid1dEntityType;
-//typedef typename Dune::YaspGrid< 2 >::template Codim< 0 >::Entity DuneYaspGrid2dEntityType;
-//typedef typename Dune::YaspGrid< 3 >::template Codim< 0 >::Entity DuneYaspGrid3dEntityType;
+typedef typename Dune::YaspGrid< 1 >::template Codim< 0 >::Entity DuneYaspGrid1dEntityType;
+typedef typename Dune::YaspGrid< 2 >::template Codim< 0 >::Entity DuneYaspGrid2dEntityType;
+typedef typename Dune::YaspGrid< 3 >::template Codim< 0 >::Entity DuneYaspGrid3dEntityType;
 
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid1dEntityType, double, 1, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid1dEntityType, double, 1, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid1dEntityType, double, 1, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid2dEntityType, double, 2, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid2dEntityType, double, 2, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid2dEntityType, double, 2, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid3dEntityType, double, 3, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid3dEntityType, double, 3, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneYaspGrid3dEntityType, double, 3, double, 3 >;
+DSF_LIST_DIMDOMAIN(DuneYaspGrid1dEntityType)
+DSF_LIST_DIMDOMAIN(DuneYaspGrid2dEntityType)
+DSF_LIST_DIMDOMAIN(DuneYaspGrid3dEntityType)
 
-//# if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
-//#   define ALUGRID_CONFORM 1
-//#   define ENABLE_ALUGRID 1
+# if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+#   define ALUGRID_CONFORM 1
+#   define ENABLE_ALUGRID 1
 
-//#   include <dune/grid/alugrid.hh>
+#   include <dune/grid/alugrid.hh>
 
-//typedef typename Dune::ALUSimplexGrid< 2, 2 >::template Codim< 0 >::Entity DuneAluSimplexGrid2dEntityType;
-//typedef typename Dune::ALUSimplexGrid< 3, 3 >::template Codim< 0 >::Entity DuneAluSimplexGrid3dEntityType;
-//typedef typename Dune::ALUCubeGrid< 3, 3 >::template Codim< 0 >::Entity DuneAluCubeGrid3dEntityType;
+typedef typename Dune::ALUSimplexGrid< 2, 2 >::template Codim< 0 >::Entity DuneAluSimplexGrid2dEntityType;
+typedef typename Dune::ALUSimplexGrid< 3, 3 >::template Codim< 0 >::Entity DuneAluSimplexGrid3dEntityType;
+typedef typename Dune::ALUCubeGrid< 3, 3 >::template Codim< 0 >::Entity DuneAluCubeGrid3dEntityType;
 
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid2dEntityType, double, 2, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid2dEntityType, double, 2, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid2dEntityType, double, 2, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid3dEntityType, double, 3, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid3dEntityType, double, 3, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneAluSimplexGrid3dEntityType, double, 3, double, 3 >;
-//template class Dune::Stuff::Function::Constant< DuneAluCubeGrid3dEntityType, double, 3, double, 1 >;
-//template class Dune::Stuff::Function::Constant< DuneAluCubeGrid3dEntityType, double, 3, double, 2 >;
-//template class Dune::Stuff::Function::Constant< DuneAluCubeGrid3dEntityType, double, 3, double, 3 >;
+DSF_LIST_DIMRANGE(DuneAluSimplexGrid2dEntityType, 2)
+DSF_LIST_DIMRANGE(DuneAluSimplexGrid3dEntityType, 3)
+DSF_LIST_DIMRANGE(DuneAluCubeGrid3dEntityType, 3)
 
-//# endif // HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
-//#endif // HAVE_DUNE_GRID
+# endif // HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+#endif // HAVE_DUNE_GRID
