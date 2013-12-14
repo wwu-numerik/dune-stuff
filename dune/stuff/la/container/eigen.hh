1c4350c772a1418c1cce8b4e8fa2e513042f47a4
Felix Albrecht <mail@felixalbrecht.de>
2013-12-14 01:28:40 +0100

    [la.container] fix cc55887

--- dune/stuff/la/container/eigen.hh
+++ dune/stuff/la/container/eigen.hh
@@ -995,7 +995,7 @@ public:
   }
 
   EigenRowMajorSparseMatrix(const size_t rr = 0, const size_t cc = 0)
-    : EigenRowMajorSparseMatrix(rr, cc)
+    : backend_(new BackendType(rr, cc))
   {}
 
   /// This constructor is needed for the python bindings.
