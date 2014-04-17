3fc1b3dd21b049da2013e6b36a6148853266f949
Tobias Leibner <tobias.leibner@googlemail.com>
2014-04-17 17:31:36 +0200

    [la.container.common] use backend() more often

--- dune/stuff/la/container/common.hh
+++ dune/stuff/la/container/common.hh
@@ -261,8 +261,7 @@ public:
       DUNE_THROW_COLORFULLY(Exceptions::shapes_do_not_match,
                             "The size of other (" << other.size() << ") does not match the size of this (" << size()
                             << ")!");
-    ensure_uniqueness();
-    backend_->operator+=(*(other.backend_));
+    backend() += *other.backend_;
   } // ... iadd(...)
 
   virtual void sub(const ThisType& other, ThisType& result) const DS_OVERRIDE DS_FINAL
@@ -286,8 +285,7 @@ public:
       DUNE_THROW_COLORFULLY(Exceptions::shapes_do_not_match,
                             "The size of other (" << other.size() << ") does not match the size of this (" << size()
                             << ")!");
-    ensure_uniqueness();
-    backend_->operator-=(*(other.backend_));
+    backend() -= *other.backend_;
   } // ... isub(...)
 
   /**
@@ -437,8 +435,7 @@ public:
 
   void scal(const ScalarType& alpha)
   {
-    ensure_uniqueness();
-    backend_->operator*=(alpha);
+    backend()*=alpha;
   } // ... scal(...)
 
   void axpy(const ScalarType& alpha, const ThisType& xx)
@@ -447,8 +444,7 @@ public:
       DUNE_THROW_COLORFULLY(Exceptions::shapes_do_not_match,
                             "The shape of xx (" << xx.rows() << "x" << xx.cols()
                             << ") does not match the shape of this (" << rows() << "x" << cols() << ")!");
-    ensure_uniqueness();
-    backend_->axpy(alpha, *(xx.backend_));
+    backend().axpy(alpha, *(xx.backend_));
   } // ... axpy(...)
 
   bool has_equal_shape(const ThisType& other) const
@@ -490,16 +486,14 @@ public:
   {
     assert(ii < rows());
     assert(jj < cols());
-    ensure_uniqueness();
-    backend_->operator[](ii)[jj] += value;
+    backend()[ii][jj] += value;
   } // ... add_to_entry(...)
 
   void set_entry(const size_t ii, const size_t jj, const ScalarType& value)
   {
     assert(ii < rows());
     assert(jj < cols());
-    ensure_uniqueness();
-    backend_->operator[](ii)[jj] = value;
+    backend()[ii][jj] = value;
   } // ... set_entry(...)
 
   ScalarType get_entry(const size_t ii, const size_t jj) const
