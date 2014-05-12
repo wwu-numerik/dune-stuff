4c0fd2a78fb83034f7ee5317b614b86c5b398984
Felix Schindler <felix@schindlerfamily.de>
2014-05-12 15:09:54 +0200

    [test.la_solver] update

--- dune/stuff/test/la_solver.cc
+++ dune/stuff/test/la_solver.cc
@@ -13,6 +13,8 @@
 #include <dune/stuff/la/container.hh>
 #include <dune/stuff/la/solver.hh>
 
+#include "la_container.hh"
+
 // toggle output
 //std::ostream& out = std::cout;
 std::ostream& out = DSC_LOG.devnull();
@@ -20,7 +22,7 @@ std::ostream& out = DSC_LOG.devnull();
 using namespace Dune::Stuff;
 using namespace Dune::Stuff::LA;
 
-typedef testing::Types< std::tuple< Dune::DynamicMatrix< double >, Dune::DynamicVector< double >, Dune::DynamicVector< double > >
+typedef testing::Types< std::tuple< CommonDenseMatrix< double >, CommonDenseVector< double >, CommonDenseVector< double > >
 #if HAVE_EIGEN
                       , std::tuple< EigenDenseMatrix< double >, EigenDenseVector< double >, EigenDenseVector< double > >
                       , std::tuple< EigenDenseMatrix< double >, EigenDenseVector< double >, EigenMappedDenseVector< double > >
@@ -46,9 +48,9 @@ struct SolverTest
   static void produces_correct_results()
   {
     const size_t dim = 10;
-    const MatrixType matrix = Container< MatrixType >::create(dim);
-    const RhsType rhs = Container< RhsType >::create(dim);
-    SolutionType solution = Container< SolutionType >::create(dim);
+    const MatrixType matrix = ContainerFactory< MatrixType >::create(dim);
+    const RhsType rhs = ContainerFactory< RhsType >::create(dim);
+    SolutionType solution = ContainerFactory< SolutionType >::create(dim);
     solution.scal(0);
 
     // dynamic test
@@ -76,7 +78,6 @@ struct SolverTest
       solver.apply(rhs, solution, detailed_opts);
       if (!solution.almost_equal(rhs))
         DUNE_THROW_COLORFULLY(Exceptions::results_are_not_as_expected, "Wrong solution!");
-      solution.scal(0);
     }
   } // ... produces_correct_results(...)
 }; // struct SolverTest
