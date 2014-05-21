00ef918e52d889fd62759093993cf32904f984f9
Felix Schindler <felix@schindlerfamily.de>
2014-05-22 00:38:11 +0200

    Revert "[functions] removed default implementation of order()"
    This reverts commit 458cadff5c7646aec900523b1f579edc06a0259b.

--- dune/stuff/functions/global.hh
+++ dune/stuff/functions/global.hh
@@ -69,6 +69,11 @@ public:
     DUNE_THROW(NotImplemented, "not needed, no meaningful default implementation possible -> exception");
   }
 
+  virtual size_t order() const
+  {
+    return std::numeric_limits<size_t>::max();
+  }
+
   virtual void evaluate(const DomainType& xx, RangeType& ret) const = 0;
 
   virtual void jacobian(const DomainType& /*x*/, JacobianRangeType& /*ret*/) const
@@ -166,6 +171,11 @@ public:
   virtual ThisType* copy() const
   {
     DUNE_THROW(NotImplemented, "not needed, no meaningful default implementation possible -> exception");
+  }
+
+  virtual size_t order() const
+  {
+    return std::numeric_limits< size_t >::max();
   }
 
   virtual void evaluate(const DomainType& x, RangeType& ret) const = 0;
