cf3a5eb05c130b0f4de8bda14f07462c21b63feb
Felix Schindler <felix.schindler@wwu.de>
2014-08-13 15:01:22 +0200

    updated project url

--- dune/stuff/test/common_ranges.cc
+++ dune/stuff/test/common_ranges.cc
@@ -1,6 +1,6 @@
 
 // This file is part of the dune-stuff project:
-//   https://users.dune-project.org/projects/dune-stuff
+//   https://github.com/wwu-numerik/dune-stuff
 // Copyright holders: Rene Milk, Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
