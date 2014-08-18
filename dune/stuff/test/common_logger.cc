0c5d8178272ae0c089c041760dc2e271e9424b1f
Tobias Leibner <tobias.leibner@googlemail.com>
2014-08-18 10:33:13 +0200

    [test.common_logger] move test from main function to TEST

--- dune/stuff/test/common_logger.cc
+++ dune/stuff/test/common_logger.cc
@@ -27,37 +27,33 @@ void do_something_that_takes_long(std::ostream& out)
   out << std::endl;
 } // void do_something_that_takes_long()
 
-int main(int argc, char** argv)
-{
-    test_init(argc, argv);
-    DSC::Logger().create(DSC::LOG_CONSOLE | DSC::LOG_ERROR);
-    DSC::Logger().error() << "This should be in output\n";
-    DSC::Logger().info() << "This should NOT be in output\n";
-    DSC::Logger().debug() << "dito\n";
-    DSC::Logger().flush();
-    for (int i: {DSC::LOG_INFO, DSC::LOG_DEBUG, DSC::LOG_ERROR}) {
-        const int id = DSC::Logger().addStream(DSC::LOG_CONSOLE | i);
-        DSC::Logger().getStream(id) << "Create a new stream with id: " << id << std::endl;
-    }
-    DSC_LOG_ERROR.suspend();
-    DSC_LOG_ERROR << "not in output\n";
-    balh(DSC_LOG_ERROR);
-    DSC_LOG_ERROR.resume();
-    DSC_LOG_ERROR << "in output\n";
-    balh(DSC_LOG_ERROR);
+TEST(LoggerTest, all) {
+  DSC::Logger().create(DSC::LOG_CONSOLE | DSC::LOG_ERROR);
+  DSC::Logger().error() << "This should be in output\n";
+  DSC::Logger().info() << "This should NOT be in output\n";
+  DSC::Logger().debug() << "dito\n";
+  DSC::Logger().flush();
+  for (int i: {DSC::LOG_INFO, DSC::LOG_DEBUG, DSC::LOG_ERROR}) {
+    const int id = DSC::Logger().addStream(DSC::LOG_CONSOLE | i);
+    DSC::Logger().getStream(id) << "Create a new stream with id: " << id << std::endl;
+  }
+  DSC_LOG_ERROR.suspend();
+  DSC_LOG_ERROR << "not in output\n";
+  balh(DSC_LOG_ERROR);
+  DSC_LOG_ERROR.resume();
+  DSC_LOG_ERROR << "in output\n";
+  balh(DSC_LOG_ERROR);
 
-    //this should do nothing whatsoever
-    balh(DSC::dev_null);
-    DSC::Logger().flush();
+  //this should do nothing whatsoever
+  balh(DSC::dev_null);
+  DSC::Logger().flush();
 
-    // this is the desired result:
-    DSC::LogStream& err = DSC::Logger().error();
-    std::cout << "begin std::cout test" << std::endl;
-    do_something_that_takes_long(std::cout);
-    std::cout << "end   std::cout test" << std::endl;
-    std::cout << "begin Logger().error() test" << std::endl;
-    do_something_that_takes_long(err);
-    std::cout << "end   Logger().error() test" << std::endl;
-
-    return 0;
+  // this is the desired result:
+  DSC::LogStream& err = DSC::Logger().error();
+  std::cout << "begin std::cout test" << std::endl;
+  do_something_that_takes_long(std::cout);
+  std::cout << "end   std::cout test" << std::endl;
+  std::cout << "begin Logger().error() test" << std::endl;
+  do_something_that_takes_long(err);
+  std::cout << "end   Logger().error() test" << std::endl;
 }
