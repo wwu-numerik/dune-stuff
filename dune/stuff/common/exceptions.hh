5bfc6355282de53b4202058c45caa4e4d1f78cbe
Felix Schindler <felix.schindler@wwu.de>
2014-08-22 16:20:16 +0200

    [common.exceptions] added test_results_missing

--- dune/stuff/common/exceptions.hh
+++ dune/stuff/common/exceptions.hh
@@ -107,6 +107,8 @@ class linear_solver_failed : public Dune::Exception {};
 
 class you_have_to_implement_this : public Dune::NotImplemented {};
 
+class test_results_missing : public Dune::NotImplemented {};
+
 
 } // namespace Exceptions
 } // namespace Stuff
