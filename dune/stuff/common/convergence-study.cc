f6ffcb84097047c1759fb0847d54cf2e121c9c10
Felix Schindler <felix.schindler@wwu.de>
2014-08-24 14:46:54 +0200

    [common.convergen-study] added optional expected_results()

--- dune/stuff/common/convergence-study.cc
+++ dune/stuff/common/convergence-study.cc
@@ -178,6 +178,12 @@ std::map< std::string, std::vector< double > > ConvergenceStudy::run(const bool 
 } // ... run(...)
 
 
+std::vector< double > ConvergenceStudy::expected_results(const std::string /*type*/) const
+{
+  DUNE_THROW(Exceptions::you_have_to_implement_this, "If you want to use this within the test suite!");
+}
+
+
 } // namespace Common
 } // namespace Stuff
 } // namespace Dune
