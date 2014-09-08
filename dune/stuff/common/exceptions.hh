587a03789d3a57ac649861fcec88845f34760e4f
Felix Schindler <felix.schindler@wwu.de>
2014-09-08 12:06:19 +0200

    [common.exceptions] marked test_results_missing as deprecated

--- dune/stuff/common/exceptions.hh
+++ dune/stuff/common/exceptions.hh
@@ -107,7 +107,9 @@ class linear_solver_failed : public Dune::Exception {};
 
 class you_have_to_implement_this : public Dune::NotImplemented {};
 
-class test_results_missing : public Dune::NotImplemented {};
+class
+  DUNE_DEPRECATED_MSG("Use something like 'EXPECT_TRUE(false) << \"test results missing for type: \" << type;' (08.09.2014)!")
+      test_results_missing : public Dune::NotImplemented {};
 
 
 } // namespace Exceptions
