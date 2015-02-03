e739d9c42f680d6c2776fa0249734aa62304158a
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 13:53:09 +0100

    [common.profiler] refs #10

--- dune/stuff/common/profiler.hh
+++ dune/stuff/common/profiler.hh
@@ -148,7 +148,7 @@ public:
 
 private:
   DatamapVector datamaps_;
-  unsigned int current_run_number_;
+  size_t current_run_number_;
   //! runtime tables etc go there
   std::string output_dir_;
   // debug counter, only outputted in debug mode
