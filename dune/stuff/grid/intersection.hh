9fe105b9dacbfce0cb4da5e12c8de518c497ec66
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 15:15:47 +0100

    [grid.intersection] refs #10

--- dune/stuff/grid/intersection.hh
+++ dune/stuff/grid/intersection.hh
@@ -72,7 +72,7 @@ void printIntersection(const IntersectionType& intersection,
 {
   out << prefix << Common::Typename< IntersectionType >::value() << std::endl;
   const auto& geometry = intersection.geometry();
-  for (int ii = 0; ii < geometry.corners(); ++ii)
+  for (decltype(geometry.corners()) ii = 0; ii < geometry.corners(); ++ii)
     Common::print(geometry.corner(ii), "corner " + Common::toString(ii), out, prefix + "  ");
 } // ... printIntersection(...)
 
