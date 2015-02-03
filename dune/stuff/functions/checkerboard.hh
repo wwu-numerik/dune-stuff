2d0c411eb064ca071323bec56b9b2518c5c10d85
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 14:34:29 +0100

    [functions.checkerboard] remove unused local typedefs

--- dune/stuff/functions/checkerboard.hh
+++ dune/stuff/functions/checkerboard.hh
@@ -38,15 +38,9 @@ class Checkerboard
   public:
     typedef typename BaseType::EntityType EntityType;
 
-    typedef typename BaseType::DomainFieldType  DomainFieldType;
-    static const unsigned int                   dimDomain = BaseType::dimDomain;
-    typedef typename BaseType::DomainType       DomainType;
-
-    typedef typename BaseType::RangeFieldType RangeFieldType;
-    static const unsigned int                 dimRange = BaseType::dimRange;
-    static const unsigned int                 dimRangeCols = BaseType::dimRangeCols;
-    typedef typename BaseType::RangeType      RangeType;
-
+    typedef typename BaseType::DomainType        DomainType;
+    typedef typename BaseType::RangeFieldType    RangeFieldType;
+    typedef typename BaseType::RangeType         RangeType;
     typedef typename BaseType::JacobianRangeType JacobianRangeType;
 
     Localfunction(const EntityType& ent, const RangeType value)
@@ -85,14 +79,9 @@ public:
 
   typedef typename BaseType::DomainFieldType  DomainFieldType;
   static const int                            dimDomain = BaseType::dimDomain;
-  typedef typename BaseType::DomainType       DomainType;
 
   typedef typename BaseType::RangeFieldType RangeFieldType;
-  static const int                          dimRange = BaseType::dimRange;
-  static const int                          dimRangeCols = BaseType::dimRangeCols;
   typedef typename BaseType::RangeType      RangeType;
-
-  typedef typename BaseType::JacobianRangeType JacobianRangeType;
 
   static const bool available = true;
 
