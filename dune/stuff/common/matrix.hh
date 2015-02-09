9252c7c7c129f327def4e134089c9e44b1b75993
Felix Schindler <felix.schindler@wwu.de>
2015-02-09 11:48:45 +0100

    [common.matrix] mark IdentityMatrix deprecated, fixes #18

--- dune/stuff/common/matrix.hh
+++ dune/stuff/common/matrix.hh
@@ -284,7 +284,9 @@ typename FieldMatrixType::field_type matrixTrace(const FieldMatrixType& matrix) 
 
 //! produces a NxN Identity matrix compatible with parent type
 template< class MatrixType >
-class IdentityMatrix
+class
+  DUNE_DEPRECATED_MSG("Will be removed soon, file an issue on https://github.com/wwu-numerik/dune-stuff/issues if you need this (09.02.2015)!")
+      IdentityMatrix
   : public MatrixType
 {
 public:
