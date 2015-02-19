dune-community/dune-xt

[grid.periodicview] do not derive PeriodicGridView from GridViewImp - derive directly from Dune::GridView instead - added suitable Traits and PeriodicGridViewImp class

dune/stuff/grid/periodicview.hh
@@ -9,14 +9,14 @@
 #define DUNE_STUFF_GRID_PERIODICVIEW_HH
 
 #include <dune/grid/common/gridview.hh>
-#include <dune/grid/common/intersection.hh>
-#include <dune/grid/common/intersectioniterator.hh>
+//#include <dune/grid/common/intersection.hh>
+//#include <dune/grid/common/intersectioniterator.hh>
 
 // includes from dune/grid/common/gridview.hh
-#include <dune/common/iteratorrange.hh>
-#include <dune/geometry/type.hh>
-#include <dune/grid/common/datahandleif.hh>
-#include <dune/grid/common/rangegenerators.hh>
+//#include <dune/common/iteratorrange.hh>
+//#include <dune/geometry/type.hh>
+//#include <dune/grid/common/datahandleif.hh>
+//#include <dune/grid/common/rangegenerators.hh>
 // ________________________
 
 
@@ -24,11 +24,11 @@ namespace Dune {
 namespace Stuff {
 namespace Grid {
 
-template <class GridViewImp>
-class PeriodicIntersection : public GridViewImp::Intersection
+template <class RealGridViewImp>
+class PeriodicIntersection : public RealGridViewImp::Intersection
 {
-  typedef PeriodicIntersection<GridViewImp> ThisType;
-  typedef typename GridViewImp::Intersection BaseType;
+  typedef PeriodicIntersection<RealGridViewImp> ThisType;
+  typedef typename RealGridViewImp::Intersection BaseType;
 
   typedef typename BaseType::LocalGeometry LocalGeometry;
   typedef typename BaseType::EntityPointer EntityPointer;
@@ -129,12 +129,12 @@ protected:
   using BaseType::real;
 };
 
-template <class GridViewImp>
-class PeriodicIntersectionIterator : public GridViewImp::IntersectionIterator
+template <class RealGridViewImp>
+class PeriodicIntersectionIterator : public RealGridViewImp::IntersectionIterator
 {
 
   // type of underlying implementation, for internal use only
-  typedef GridViewImp GridViewType;
+  typedef RealGridViewImp GridViewType;
   typedef PeriodicIntersectionIterator<GridViewType> ThisType;
   typedef typename GridViewType::IntersectionIterator BaseType;
   typedef PeriodicIntersection<GridViewType> Intersection;
@@ -145,9 +145,18 @@ public:
   /** Copy Constructor from IntersectionIteratorImp */
   PeriodicIntersectionIterator(BaseType& real_intersection_iterator)
     : BaseType(real_intersection_iterator)
+    , real_intersection_iterator_(real_intersection_iterator)
     , current_intersection_(((BaseType*)this)->operator*())
   {
   }
+
+  PeriodicIntersectionIterator(BaseType real_intersection_iterator)
+    : BaseType(real_intersection_iterator)
+    , real_intersection_iterator_(real_intersection_iterator)
+    , current_intersection_(((BaseType*)this)->operator*())
+  {
+  }
+
 
   //===========================================================
   /** @name Dereferencing
@@ -210,61 +219,233 @@ public:
 
 private:
   Intersection current_intersection_;
+  BaseType real_intersection_iterator_;
 };
 
-template <class GridViewImp>
-class PeriodicGridView : public GridViewImp
+// forward
+template <class RealGridViewImp>
+class PeriodicGridViewImp;
+
+template <class RealGridViewImp>
+class PeriodicGridViewTraits
 {
-  typedef PeriodicGridView<GridViewImp> ThisType;
-  typedef GridViewImp BaseType;
+public:
+  typedef RealGridViewImp RealGridViewType;
+  typedef PeriodicGridViewImp<RealGridViewType> GridViewImp;
+  typedef typename RealGridViewType::Grid Grid;
+  typedef typename RealGridViewType::IndexSet IndexSet;
+  typedef typename RealGridViewType::CollectiveCommunication CollectiveCommunication;
+
+  typedef PeriodicIntersection<RealGridViewType> Intersection;
+  typedef PeriodicIntersectionIterator<RealGridViewType> IntersectionIterator;
+
+  typedef typename RealGridViewType::Traits RealGridViewTraits;
+
+  /** \brief A struct that collects all associated types of one implementation
+             from the Traits class.
+   */
+  template <int cd>
+  struct Codim
+  {
+    /** \brief type of iterator returned by the grid view */ // replace this at least for cd == 1 later
+    typedef typename RealGridViewTraits::template Codim<cd>::Iterator Iterator;
+
+    /** \brief type of corresponding entity pointer */
+    typedef typename RealGridViewTraits::template Codim<cd>::EntityPointer EntityPointer;
+
+    /** \brief type of corresponding entity */
+    typedef typename RealGridViewTraits::template Codim<cd>::Entity Entity;
+
+    /** \brief type of the geometry implementation */
+    typedef typename RealGridViewTraits::template Codim<cd>::Geometry Geometry;
+
+    /** \brief type of the implementation for local geometries */
+    typedef typename RealGridViewTraits::template Codim<cd>::LocalGeometry LocalGeometry;
+
+    /** \brief Define types needed to iterate over entities of a given partition type */
+    template <PartitionIteratorType pit>
+    struct Partition
+    {
+      /** \brief iterator over a given codim and partition type */
+      typedef typename RealGridViewTraits::template Codim<cd>::template Partition<pit>::Iterator Iterator;
+    };
+  }; //: public Traits :: template Codim<cd> {};
+
+  enum
+  {
+    /** \brief Export if this grid view is conforming */
+    conforming = RealGridViewTraits::conforming
+  };
+
+  /** \brief type used for coordinates in grid */
+  typedef typename Grid::ctype ctype;
+
+  enum
+  { //! \brief The dimension of the grid
+    dimension = Grid::dimension
+  };
+
+  enum
+  { //! \brief The dimension of the world the grid lives in
+    dimensionworld = Grid::dimensionworld
+  };
+};
+
+
+template <class RealGridViewImp>
+class PeriodicGridViewImp
+{
+  typedef RealGridViewImp RealGridViewType;
+  typedef PeriodicGridViewImp<RealGridViewType> ThisType;
+  typedef PeriodicGridViewTraits<RealGridViewType> Traits;
 
 public:
-  typedef typename BaseType::Grid Grid;
-  typedef typename BaseType::IndexSet IndexSet;
+  typedef typename RealGridViewType::Grid Grid;
+  typedef typename RealGridViewType::IndexSet IndexSet;
   typedef typename Dune::GeometryType GeometryType;
-  typedef PeriodicIntersectionIterator<BaseType> IntersectionIterator;
-  typedef PeriodicIntersection<BaseType> Intersection;
-  typedef typename BaseType::CollectiveCommunication CollectiveCommunication;
+  typedef PeriodicIntersectionIterator<RealGridViewType> IntersectionIterator;
+  typedef PeriodicIntersection<RealGridViewType> Intersection;
+  typedef typename RealGridViewType::CollectiveCommunication CollectiveCommunication;
+
+  /** \brief Codim Structure */
+  template <int cd>
+  struct Codim : public Traits::template Codim<cd>
+  {
+  };
 
   /** \brief constructor (engine concept) */
-  PeriodicGridView(const BaseType& real_grid_view)
-    : BaseType(real_grid_view)
+  PeriodicGridViewImp(const RealGridViewType& real_grid_view)
+    : real_grid_view_(real_grid_view)
   {
   }
 
-  //  /** \brief Copy constructor */
-  //  PeriodicGridView ( const ThisType &other )
-  //    : BaseType( (BaseType&)other )
-  //  {}
-
-  /** \brief assignment operator */
-  ThisType& operator=(const ThisType& other)
+  /** \brief obtain a const reference to the underlying hierarchic grid */
+  const Grid& grid() const
   {
-    impl_ = other.impl_;
-    return *this;
+    return real_grid_view_.grid();
   }
 
-public:
+  /** \brief obtain the index set
+   *
+   * The lifetime of the returned index set is bound to the lifetime of the
+   * grid view. Keep a copy of the grid view to prevent the index set from
+   * becoming a dangling reference.
+   */
+  const IndexSet& indexSet() const
+  {
+    return real_grid_view_.indexSet();
+  }
+
+  /** \brief obtain number of entities in a given codimension */
+  int size(int codim) const
+  {
+    return real_grid_view_.size(codim);
+  }
+
+  /** \brief obtain number of entities with a given geometry type */
+  int size(const GeometryType& type) const
+  {
+    return real_grid_view_.size(type);
+  }
+
+  /** @brief Return true if the given entity is contained in this grid view
+   * @todo Currently we call the implementation on the IndexSet.  This may lead to suboptimal efficiency.
+   *
+   * \note If the input element e is not an element of the grid, then
+   *       the result of contains() is undefined.
+   */
+  template <class EntityType>
+  bool contains(const EntityType& e) const
+  {
+    return real_grid_view_.indexSet().contains(e);
+  }
+
+  /** \brief obtain begin iterator for this view */
+  template <int cd>
+  typename Codim<cd>::Iterator begin() const
+  {
+    return real_grid_view_.template begin<cd>();
+  }
+
+  /** \brief obtain end iterator for this view */
+  template <int cd>
+  typename Codim<cd>::Iterator end() const
+  {
+    return real_grid_view_.template end<cd>();
+  }
+
+  /** \brief obtain begin iterator for this view */
+  template <int cd, PartitionIteratorType pitype>
+  typename Codim<cd>::template Partition<pitype>::Iterator begin() const
+  {
+    return real_grid_view_.template begin<cd, pitype>();
+  }
+
+  /** \brief obtain end iterator for this view */
+  template <int cd, PartitionIteratorType pitype>
+  typename Codim<cd>::template Partition<pitype>::Iterator end() const
+  {
+    return real_grid_view_.template end<cd, pitype>();
+  }
+
   /** \brief obtain begin intersection iterator with respect to this view */
-  IntersectionIterator ibegin(const typename BaseType::template Codim<0>::Entity& entity) const
+  IntersectionIterator ibegin(const typename Codim<0>::Entity& entity) const
   {
-    auto real_intersection_iterator      = ((BaseType*)this)->ibegin(entity);
-    auto& real_intersection_iterator_ref = real_intersection_iterator;
-    IntersectionIterator periodic_intersection_iterator(real_intersection_iterator_ref);
-    return periodic_intersection_iterator;
+    return IntersectionIterator(real_grid_view_.ibegin(entity));
   }
 
   /** \brief obtain end intersection iterator with respect to this view */
-  IntersectionIterator iend(const typename BaseType::template Codim<0>::Entity& entity) const
+  IntersectionIterator iend(const typename Codim<0>::Entity& entity) const
   {
-    auto real_intersection_iterator      = ((BaseType*)this)->iend(entity);
-    auto& real_intersection_iterator_ref = real_intersection_iterator;
-    IntersectionIterator periodic_intersection_iterator(real_intersection_iterator_ref);
-    return periodic_intersection_iterator;
+    return IntersectionIterator(real_grid_view_.iend(entity));
   }
 
-protected:
-  using BaseType::impl_;
+  /** \brief obtain collective communication object */
+  const CollectiveCommunication& comm() const
+  {
+    return real_grid_view_.comm();
+  }
+
+  /** \brief Return size of the overlap region for a given codim on the grid view.  */
+  int overlapSize(int codim) const
+  {
+    return real_grid_view_.overlapSize(codim);
+  }
+
+  /** \brief Return size of the ghost region for a given codim on the grid view.  */
+  int ghostSize(int codim) const
+  {
+    return real_grid_view_.ghostSize(codim);
+  }
+
+  /** \brief Communicate data on this view */
+  template <class DataHandleImp, class DataType>
+  void communicate(CommDataHandleIF<DataHandleImp, DataType>& data, InterfaceType iftype,
+                   CommunicationDirection dir) const
+  {
+    real_grid_view_.communicate(data, iftype, dir);
+  }
+
+private:
+  const RealGridViewType& real_grid_view_;
+};
+
+template <class RealGridViewImp>
+class PeriodicGridView : public Dune::GridView<PeriodicGridViewTraits<RealGridViewImp>>
+{
+  typedef RealGridViewImp RealGridViewType;
+  typedef typename Dune::GridView<PeriodicGridViewTraits<RealGridViewType>> BaseType;
+  typedef PeriodicGridViewImp<RealGridViewType> PeriodicDings;
+
+public:
+  PeriodicGridView(const RealGridViewType& real_grid_view)
+    : BaseType(*(new PeriodicDings(real_grid_view)))
+    , periodic_grid_view_(real_grid_view)
+  {
+  }
+
+private:
+  const PeriodicGridViewImp<RealGridViewType>& periodic_grid_view_;
 };
 
 } // namespace Grid

wwu-numerik/dune-stuff

dune/stuff/grid/periodicview.hh
@@ -9,14 +9,14 @@
 #define DUNE_STUFF_GRID_PERIODICVIEW_HH
 
 #include <dune/grid/common/gridview.hh>
-#include <dune/grid/common/intersection.hh>
-#include <dune/grid/common/intersectioniterator.hh>
+//#include <dune/grid/common/intersection.hh>
+//#include <dune/grid/common/intersectioniterator.hh>
 
 // includes from dune/grid/common/gridview.hh
-#include <dune/common/iteratorrange.hh>
-#include <dune/geometry/type.hh>
-#include <dune/grid/common/datahandleif.hh>
-#include <dune/grid/common/rangegenerators.hh>
+//#include <dune/common/iteratorrange.hh>
+//#include <dune/geometry/type.hh>
+//#include <dune/grid/common/datahandleif.hh>
+//#include <dune/grid/common/rangegenerators.hh>
 // ________________________
 
 
@@ -24,12 +24,12 @@ namespace Dune {
 namespace Stuff {
 namespace Grid {
 
-template< class GridViewImp >
+template< class RealGridViewImp >
 class PeriodicIntersection
-    : public GridViewImp::Intersection
+    : public RealGridViewImp::Intersection
 {
-  typedef PeriodicIntersection < GridViewImp > ThisType;
-  typedef typename GridViewImp::Intersection   BaseType;
+  typedef PeriodicIntersection < RealGridViewImp > ThisType;
+  typedef typename RealGridViewImp::Intersection   BaseType;
 
   typedef typename BaseType::LocalGeometry LocalGeometry;
   typedef typename BaseType::EntityPointer EntityPointer;
@@ -127,13 +127,13 @@ protected:
   using BaseType::real;
 };
 
-template< class GridViewImp >
+template< class RealGridViewImp >
 class PeriodicIntersectionIterator
-    : public GridViewImp::IntersectionIterator
+    : public RealGridViewImp::IntersectionIterator
 {
 
   // type of underlying implementation, for internal use only
-  typedef GridViewImp GridViewType;
+  typedef RealGridViewImp GridViewType;
   typedef PeriodicIntersectionIterator< GridViewType > ThisType;
   typedef typename GridViewType::IntersectionIterator BaseType;
   typedef PeriodicIntersection< GridViewType > Intersection;
@@ -144,8 +144,16 @@ public:
   /** Copy Constructor from IntersectionIteratorImp */
   PeriodicIntersectionIterator ( BaseType& real_intersection_iterator )
     : BaseType( real_intersection_iterator )
+    , real_intersection_iterator_(real_intersection_iterator)
     , current_intersection_(((BaseType*)this)->operator*())
   {}
+
+  PeriodicIntersectionIterator ( BaseType real_intersection_iterator )
+    : BaseType( real_intersection_iterator )
+    , real_intersection_iterator_(real_intersection_iterator)
+    , current_intersection_(((BaseType*)this)->operator*())
+  {}
+
 
   //===========================================================
   /** @name Dereferencing
@@ -208,63 +216,232 @@ public:
 
 private:
   Intersection current_intersection_;
+  BaseType real_intersection_iterator_;
 };
 
-template< class GridViewImp >
-class PeriodicGridView
-    : public GridViewImp
+//forward
+template < class RealGridViewImp >
+class PeriodicGridViewImp;
+
+template < class RealGridViewImp >
+class PeriodicGridViewTraits
 {
-  typedef PeriodicGridView< GridViewImp > ThisType;
-  typedef GridViewImp BaseType;
+public:
+  typedef RealGridViewImp RealGridViewType;
+  typedef PeriodicGridViewImp < RealGridViewType > GridViewImp;
+  typedef typename RealGridViewType::Grid Grid;
+  typedef typename RealGridViewType::IndexSet IndexSet;
+  typedef typename RealGridViewType::CollectiveCommunication CollectiveCommunication;
+
+  typedef PeriodicIntersection < RealGridViewType > Intersection;
+  typedef PeriodicIntersectionIterator < RealGridViewType > IntersectionIterator;
+
+  typedef typename RealGridViewType::Traits RealGridViewTraits;
+
+  /** \brief A struct that collects all associated types of one implementation
+             from the Traits class.
+   */
+  template< int cd >
+  struct Codim {
+    /** \brief type of iterator returned by the grid view */ //replace this at least for cd == 1 later
+    typedef typename RealGridViewTraits :: template Codim<cd> :: Iterator Iterator;
+
+    /** \brief type of corresponding entity pointer */
+    typedef typename RealGridViewTraits :: template Codim<cd> :: EntityPointer EntityPointer;
+
+    /** \brief type of corresponding entity */
+    typedef typename RealGridViewTraits :: template Codim<cd> :: Entity Entity;
+
+    /** \brief type of the geometry implementation */
+    typedef typename RealGridViewTraits :: template Codim<cd> :: Geometry Geometry;
+
+    /** \brief type of the implementation for local geometries */
+    typedef typename RealGridViewTraits :: template Codim<cd> :: LocalGeometry LocalGeometry;
+
+    /** \brief Define types needed to iterate over entities of a given partition type */
+    template< PartitionIteratorType pit >
+    struct Partition
+    {
+      /** \brief iterator over a given codim and partition type */
+      typedef typename RealGridViewTraits :: template Codim< cd >
+      :: template Partition< pit > :: Iterator Iterator;
+    };
+  }; //: public Traits :: template Codim<cd> {};
+
+  enum {
+    /** \brief Export if this grid view is conforming */
+    conforming = RealGridViewTraits :: conforming
+  };
+
+  /** \brief type used for coordinates in grid */
+  typedef typename Grid::ctype ctype;
+
+  enum { //! \brief The dimension of the grid
+    dimension = Grid :: dimension
+  };
+
+  enum { //! \brief The dimension of the world the grid lives in
+    dimensionworld = Grid :: dimensionworld
+  };
+};
+
+
+template< class RealGridViewImp >
+class PeriodicGridViewImp
+{
+  typedef RealGridViewImp RealGridViewType;
+  typedef PeriodicGridViewImp< RealGridViewType > ThisType;
+  typedef PeriodicGridViewTraits< RealGridViewType > Traits;
 
 public:
-  typedef typename BaseType::Grid   Grid;
-  typedef typename BaseType::IndexSet IndexSet;
+  typedef typename RealGridViewType::Grid   Grid;
+  typedef typename RealGridViewType::IndexSet IndexSet;
   typedef typename Dune::GeometryType GeometryType;
-  typedef PeriodicIntersectionIterator< BaseType > IntersectionIterator;
-  typedef PeriodicIntersection< BaseType > Intersection;
-  typedef typename BaseType::CollectiveCommunication CollectiveCommunication;
+  typedef PeriodicIntersectionIterator< RealGridViewType > IntersectionIterator;
+  typedef PeriodicIntersection< RealGridViewType > Intersection;
+  typedef typename RealGridViewType::CollectiveCommunication CollectiveCommunication;
+
+  /** \brief Codim Structure */
+  template< int cd >
+  struct Codim : public Traits::template Codim<cd> {};
 
   /** \brief constructor (engine concept) */
-  PeriodicGridView ( const BaseType& real_grid_view )
-    : BaseType(real_grid_view)
+  PeriodicGridViewImp ( const RealGridViewType& real_grid_view )
+    : real_grid_view_(real_grid_view)
   {}
 
-//  /** \brief Copy constructor */
-//  PeriodicGridView ( const ThisType &other )
-//    : BaseType( (BaseType&)other )
-//  {}
-
-  /** \brief assignment operator */
-  ThisType &operator= ( const ThisType &other )
+  /** \brief obtain a const reference to the underlying hierarchic grid */
+  const Grid &grid () const
   {
-    impl_ = other.impl_;
-    return *this;
+    return real_grid_view_.grid();
   }
 
-public:
+  /** \brief obtain the index set
+   *
+   * The lifetime of the returned index set is bound to the lifetime of the
+   * grid view. Keep a copy of the grid view to prevent the index set from
+   * becoming a dangling reference.
+   */
+  const IndexSet &indexSet () const
+  {
+    return real_grid_view_.indexSet();
+  }
+
+  /** \brief obtain number of entities in a given codimension */
+  int size ( int codim ) const
+  {
+    return real_grid_view_.size( codim );
+  }
+
+  /** \brief obtain number of entities with a given geometry type */
+  int size ( const GeometryType &type ) const
+  {
+    return real_grid_view_.size( type );
+  }
+
+  /** @brief Return true if the given entity is contained in this grid view
+   * @todo Currently we call the implementation on the IndexSet.  This may lead to suboptimal efficiency.
+   *
+   * \note If the input element e is not an element of the grid, then
+   *       the result of contains() is undefined.
+   */
+  template<class EntityType>
+  bool contains (const EntityType& e) const
+  {
+    return real_grid_view_.indexSet().contains(e);
+  }
+
+  /** \brief obtain begin iterator for this view */
+  template< int cd >
+  typename Codim< cd > :: Iterator begin () const
+  {
+    return real_grid_view_.template begin<cd>();
+  }
+
+  /** \brief obtain end iterator for this view */
+  template< int cd >
+  typename Codim< cd > :: Iterator end () const
+  {
+    return real_grid_view_.template end<cd>();
+  }
+
+  /** \brief obtain begin iterator for this view */
+  template< int cd , PartitionIteratorType pitype >
+  typename Codim< cd > :: template Partition< pitype > :: Iterator
+  begin () const
+  {
+    return real_grid_view_.template begin<cd,pitype>();
+  }
+
+  /** \brief obtain end iterator for this view */
+  template< int cd, PartitionIteratorType pitype >
+  typename Codim< cd > :: template Partition< pitype > :: Iterator
+  end () const
+  {
+    return real_grid_view_.template end<cd,pitype>();
+  }
+
   /** \brief obtain begin intersection iterator with respect to this view */
   IntersectionIterator
-  ibegin ( const typename BaseType::template Codim< 0 >::Entity& entity ) const
+  ibegin ( const typename Codim< 0 > :: Entity &entity ) const
   {
-    auto real_intersection_iterator = ((BaseType*)this)->ibegin(entity);
-    auto& real_intersection_iterator_ref = real_intersection_iterator;
-    IntersectionIterator periodic_intersection_iterator(real_intersection_iterator_ref);
-    return periodic_intersection_iterator;
+    return IntersectionIterator(real_grid_view_.ibegin(entity));
   }
 
   /** \brief obtain end intersection iterator with respect to this view */
   IntersectionIterator
-  iend ( const typename BaseType::template Codim< 0 > :: Entity &entity ) const
+  iend ( const typename Codim< 0 > :: Entity &entity ) const
   {
-    auto real_intersection_iterator = ((BaseType*)this)->iend(entity);
-    auto& real_intersection_iterator_ref = real_intersection_iterator;
-    IntersectionIterator periodic_intersection_iterator(real_intersection_iterator_ref);
-    return periodic_intersection_iterator;
+    return IntersectionIterator(real_grid_view_.iend(entity));
   }
 
-protected:
-  using BaseType::impl_;
+  /** \brief obtain collective communication object */
+  const CollectiveCommunication &comm () const
+  {
+    return real_grid_view_.comm();
+  }
+
+  /** \brief Return size of the overlap region for a given codim on the grid view.  */
+  int overlapSize(int codim) const
+  {
+    return real_grid_view_.overlapSize(codim);
+  }
+
+  /** \brief Return size of the ghost region for a given codim on the grid view.  */
+  int ghostSize(int codim) const
+  {
+    return real_grid_view_.ghostSize(codim);
+  }
+
+  /** \brief Communicate data on this view */
+  template< class DataHandleImp, class DataType >
+  void communicate ( CommDataHandleIF< DataHandleImp, DataType > &data,
+                     InterfaceType iftype,
+                     CommunicationDirection dir ) const
+  {
+    real_grid_view_.communicate(data,iftype,dir);
+  }
+
+private:
+  const RealGridViewType& real_grid_view_;
+};
+
+template< class RealGridViewImp >
+class PeriodicGridView
+    : public Dune::GridView< PeriodicGridViewTraits < RealGridViewImp > >
+{
+  typedef RealGridViewImp RealGridViewType;
+  typedef typename Dune::GridView < PeriodicGridViewTraits < RealGridViewType > > BaseType;
+  typedef PeriodicGridViewImp< RealGridViewType > PeriodicDings;
+
+public:
+  PeriodicGridView( const RealGridViewType& real_grid_view )
+      : BaseType(*(new PeriodicDings(real_grid_view)))
+      , periodic_grid_view_(real_grid_view)
+{}
+
+  private:
+    const PeriodicGridViewImp< RealGridViewType >& periodic_grid_view_;
 };
 
 } // namespace Grid