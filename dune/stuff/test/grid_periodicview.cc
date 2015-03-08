eec6c8abe542c0a2a3a650f541c36538c4999714
Felix Schindler <felix.schindler@wwu.de>
2015-03-08 11:52:22 +0100

    [grid.periodicview] move all but PeriodicGridView to internal namespace

--- dune/stuff/test/grid_periodicview.cc
+++ dune/stuff/test/grid_periodicview.cc
@@ -53,9 +53,9 @@ struct PeriodicViewTestCube
   typedef typename Dune::Stuff::Grid::template PeriodicGridView< GridViewType >      PeriodicGridViewType;
   typedef typename PeriodicGridViewType::template Codim< 0 >::Entity    EntityType;
   typedef typename PeriodicGridViewType::template Codim< 0 >::Iterator  EntityIteratorType;
-  typedef typename Dune::Stuff::Grid::template PeriodicIntersectionIterator< GridViewType >   PeriodicIntersectionIteratorType;
-  typedef typename Dune::Stuff::Grid::template PeriodicIntersection< GridViewType >           PeriodicIntersectionType;
-  typedef typename PeriodicIntersectionType::EntityPointer                                    EntityPointerType;
+  typedef typename Dune::Stuff::Grid::internal::template PeriodicIntersectionIterator< GridViewType > PeriodicIntersectionIteratorType;
+  typedef typename Dune::Stuff::Grid::internal::template PeriodicIntersection< GridViewType >         PeriodicIntersectionType;
+  typedef typename PeriodicIntersectionType::EntityPointer                                            EntityPointerType;
   typedef typename GridViewType::CollectiveCommunication  CollectiveCommunication;
   static const size_t dimDomain = GridViewType::dimension;
 
