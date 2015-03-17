ccdb90144905a67095d612cf66a3b423a58cf3a4
Rene Milk <rene.milk@uni-muenster.de>
2015-03-17 15:44:11 +0100

    [common.exceptions] removes deprecated test_results_missing

--- dune/stuff/common/exceptions.hh
+++ dune/stuff/common/exceptions.hh
@@ -107,11 +107,6 @@ class linear_solver_failed : public Dune::Exception {};
 
 class you_have_to_implement_this : public Dune::NotImplemented {};
 
-class
-  DUNE_DEPRECATED_MSG("Use something like 'EXPECT_TRUE(false) << \"test results missing for type: \" << type;' (08.09.2014)!")
-      test_results_missing : public Dune::NotImplemented {};
-
-
 } // namespace Exceptions
 } // namespace Stuff
 } // namespace Dune
