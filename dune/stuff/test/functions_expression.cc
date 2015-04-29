510dcd1f9f714acfb05521fcd8880ef8aeb30bbe
Tobias Leibner <tobias.leibner@googlemail.com>
2015-04-29 14:33:40 +0200

    [functions.expression] remove ctors and build_gradient taking FieldMatrix

--- dune/stuff/test/functions_expression.cc
+++ dune/stuff/test/functions_expression.cc
@@ -34,13 +34,10 @@
     void check() const \
     { \
       Dune::Stuff::Common::Configuration config = LocalizableFunctionType::default_config(); \
-      const std::unique_ptr< const LocalizableFunctionType > \
-          function(LocalizableFunctionType::create(config)); \
+      const std::unique_ptr< const LocalizableFunctionType > function(LocalizableFunctionType::create(config)); \
       config["expression"] = "[2*x[0] 2*x[1] 2*x[2]]"; \
       config["gradient"] = "[2 0 0; 0 2 0; 0 0 2]"; \
-      const std::unique_ptr< const LocalizableFunctionType > \
-          function2(LocalizableFunctionType::create(config)); \
-      const LocalizableFunctionType function3("x", {"x[0]", "x[0]", "x[0]"}); \
+      const std::unique_ptr< const LocalizableFunctionType > function2(LocalizableFunctionType::create(config)); \
     } \
   };
 // TEST_STRUCT_GENERATOR
