c402e86956017b5426b7cd1c9fb7ac4de4e6c13b
b_verf01 <b_verf01@uni-muenster.de>
2015-05-11 15:15:35 +0200

    make the use of complex-valued vectors possible by adapting the return type and some methods; almostequal in the vector interface does not work yet

--- dune/stuff/la/container/vector-interface.hh
+++ dune/stuff/la/container/vector-interface.hh
@@ -12,8 +12,11 @@
 #include <limits>
 #include <iostream>
 #include <vector>
+#include <complex>
 
 #include <boost/numeric/conversion/cast.hpp>
+
+#include<dune/common/ftraits.hh>
 
 #include <dune/stuff/common/crtp.hh>
 #include <dune/stuff/common/exceptions.hh>
@@ -48,7 +51,8 @@ class VectorInterface
 {
 public:
   typedef typename Traits::derived_type derived_type;
-  typedef ScalarImp ScalarType;
+  typedef typename Dune::FieldTraits< ScalarImp >::field_type ScalarType;
+  typedef typename Dune::FieldTraits< ScalarImp >::real_type RealScalarType;
 
   typedef internal::VectorInputIterator< Traits, ScalarType >  const_iterator;
   typedef internal::VectorOutputIterator< Traits, ScalarType > iterator;
@@ -119,7 +123,7 @@ public:
   virtual bool valid() const
   {
     for (const auto& val : *this) {
-      if (std::isnan(val) || std::isinf(val))
+      if (std::isnan(std::real(val)) || std::isnan(std::imag(val)) || std::isinf(std::abs(val)))
         return false;
     }
     return true;
@@ -165,9 +169,9 @@ public:
    *  \return A pair of the lowest index at which the maximum is attained and the absolute maximum value.
    *  \note   If you override this method please use exceptions instead of assertions (for the python bindings).
    */
-  virtual std::pair< size_t, ScalarType > amax() const
+  virtual std::pair< size_t, RealScalarType > amax() const
   {
-    auto result = std::make_pair(size_t(0), ScalarType(0));
+    auto result = std::make_pair(size_t(0), RealScalarType(0));
     for (size_t ii = 0; ii < size(); ++ii) {
       const auto value = std::abs(get_entry_ref(ii));
       if (value > result.second) {
@@ -188,7 +192,7 @@ public:
    *  \note   If you override this method please use exceptions instead of assertions (for the python bindings).
    */
   virtual bool almost_equal(const derived_type& other,
-                            const ScalarType epsilon = Stuff::Common::FloatCmp::DefaultEpsilon< ScalarType >::value()) const
+                            const RealScalarType epsilon = Stuff::Common::FloatCmp::DefaultEpsilon< RealScalarType >::value()) const
   {
     if (other.size() != size())
       DUNE_THROW(Exceptions::shapes_do_not_match,
@@ -207,7 +211,7 @@ public:
    */
   template< class T >
   bool almost_equal(const VectorInterface< T >& other,
-                    const ScalarType epsilon = Stuff::Common::FloatCmp::DefaultEpsilon< ScalarType >::value()) const
+                    const RealScalarType epsilon = Stuff::Common::FloatCmp::DefaultEpsilon< RealScalarType >::value()) const
   {
     if (other.size() != size())
       DUNE_THROW(Exceptions::shapes_do_not_match,
@@ -230,7 +234,7 @@ public:
                             << ")!");
     ScalarType result = 0;
     for (size_t ii = 0; ii < size(); ++ii)
-      result += get_entry_ref(ii) * other.get_entry_ref(ii);
+      result += std::conj(get_entry_ref(ii)) * other.get_entry_ref(ii);
     return result;
   } // ... dot(...)
 
@@ -239,9 +243,9 @@ public:
    *  \return The l1-norm of the vector.
    *  \note   If you override this method please use exceptions instead of assertions (for the python bindings).
    */
-  virtual ScalarType l1_norm() const
+  virtual RealScalarType l1_norm() const
   {
-    ScalarType result = 0;
+    RealScalarType result = 0;
     for (size_t ii = 0; ii < size(); ++ii)
       result += std::abs(get_entry_ref(ii));
     return result;
@@ -252,9 +256,10 @@ public:
    *  \return The l2-norm of the vector.
    *  \note   If you override this method please use exceptions instead of assertions (for the python bindings).
    */
-  virtual ScalarType l2_norm() const
+  virtual RealScalarType l2_norm() const
   {
-    return std::sqrt(dot(this->as_imp(*this)));
+    return std::sqrt(std::abs(dot(this->as_imp(*this)))); //std::abs is only needed for the right return type:
+                                                         //v.dot(v) should always be a ScalarType with zero imaginary part
   }
 
   /**
@@ -262,7 +267,7 @@ public:
    *  \return The l-infintiy-norm of the vector.
    *  \note   If you override this method please use exceptions instead of assertions (for the python bindings).
    */
-  virtual ScalarType sup_norm() const
+  virtual RealScalarType sup_norm() const
   {
     return amax().second;
   }
@@ -550,15 +555,15 @@ public:
    * \brief Variant of amax() needed for the python bindings.
    * \see   amax()
    */
-  std::vector< ScalarType > pb_amax() const
+  std::vector< RealScalarType > pb_amax() const
   {
     const auto max = amax();
     try {
-      return { boost::numeric_cast< ScalarType >(max.first), max.second};
+      return { boost::numeric_cast< RealScalarType >(max.first), max.second};
     } catch (boost::bad_numeric_cast& ee) {
       DUNE_THROW(Exceptions::external_error,
                  "There was an error in boost converting '" << max.first << "' to '"
-                 << Common::Typename< ScalarType >::value() << "': " << ee.what());
+                 << Common::Typename< RealScalarType >::value() << "': " << ee.what());
     }
   } // ... pb_amax(...)
 
