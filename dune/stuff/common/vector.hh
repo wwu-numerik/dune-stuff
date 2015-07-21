605386ecdcbe95962e7082dc48fe601380a0c82e
Rene Milk <rene.milk@uni-muenster.de>
2015-07-21 16:03:29 +0200

    [common] is_vector<std::complex<T>> is now false
    closes #63

--- dune/stuff/common/vector.hh
+++ dune/stuff/common/vector.hh
@@ -160,7 +160,7 @@ struct VectorAbstraction< std::complex< T > >
   typedef ScalarType S;
   typedef RealType   R;
 
-  static const bool is_vector = true;
+  static const bool is_vector = false;
 
   static const bool has_static_size = true;
 
@@ -182,7 +182,7 @@ struct VectorAbstraction< std::complex< T > >
 };
 
 template< class VectorType >
-    typename std::enable_if< is_vector< VectorType >::value && !is_complex<VectorType>::value, VectorType >::type
+    typename std::enable_if< is_vector< VectorType >::value, VectorType >::type
 create(const size_t sz,
        const typename VectorAbstraction< VectorType >::S& val = typename VectorAbstraction< VectorType >::S(0))
 {
@@ -190,7 +190,7 @@ create(const size_t sz,
 }
 
 template< class T, class SR >
-  typename std::enable_if< is_vector< T >::value && is_complex<T>::value, T >::type
+  typename std::enable_if< is_complex<T>::value, T >::type
 create(const size_t /*sz*/,
        const SR& val = typename VectorAbstraction< T >::R(0))
 {
