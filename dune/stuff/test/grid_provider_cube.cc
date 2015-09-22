ad3d539b90178d0bcde6911a2f51880bf81e0ff1
Felix Schindler <felix.schindler@wwu.de>
2015-09-22 15:55:03 +0200

    [test.grid_provider] split into header and source

---
+++ dune/stuff/test/grid_provider_cube.cc
@@ -0,0 +1,62 @@
+// This file is part of the dune-stuff project:
+//   https://github.com/wwu-numerik/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include "main.hxx"
+
+#include <dune/stuff/grid/provider/cube.hh>
+
+#include "grid_provider.hh"
+
+#if HAVE_DUNE_GRID
+
+template< class GridType >
+struct CubeGridProvider
+  : public GridProviderBase< Dune::Stuff::Grid::Providers::Cube< GridType > >
+{};
+
+
+typedef testing::Types< SGRIDS
+                      , YASPGRIDS
+# if HAVE_ALUGRID
+                      , ALUGRIDS
+# endif
+                      > GridTypes;
+
+TYPED_TEST_CASE(CubeGridProvider, GridTypes);
+TYPED_TEST(CubeGridProvider, is_default_creatable)
+{
+  this->is_default_creatable();
+}
+TYPED_TEST(CubeGridProvider, fulfills_const_interface)
+{
+  this->const_interface();
+}
+TYPED_TEST(CubeGridProvider, is_visualizable)
+{
+  this->visualize();
+}
+
+# if HAVE_DUNE_FEM
+
+TYPED_TEST(CubeGridProvider, fulfills_non_const_interface)
+{
+  this->non_const_interface();
+}
+
+# else // HAVE_DUNE_FEM
+
+TEST(DISABLED_CubeGridProvider, fulfills_non_const_interface) {}
+
+# endif
+
+
+#else // HAVE_DUNE_GRID
+
+TEST(DISABLED_CubeGridProvider, is_default_creatable) {}
+TEST(DISABLED_CubeGridProvider, fulfills_const_interface) {}
+TEST(DISABLED_CubeGridProvider, is_visualizable) {}
+
+#endif // HAVE_DUNE_GRID
+
