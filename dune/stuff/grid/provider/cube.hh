ced5dcb37e835d88d8acbcad41bdb589307939d2
Felix Schindler <felix.schindler@wwu.de>
2015-10-01 08:31:59 +0200

    [grid.provider.cube] fix default ctor

--- dune/stuff/grid/provider/cube.hh
+++ dune/stuff/grid/provider/cube.hh
@@ -190,12 +190,12 @@ public:
    *  \param[in]  num_elements (optional)
    *              Number of elements.
    **/
-  explicit Cube(const DomainFieldType lower_left = default_config().get< DomainFieldType >("lower_left"),
-       const DomainFieldType upper_right = default_config().get< DomainFieldType >("upper_right"),
-       const unsigned int num_elements = default_config().get< std::vector< unsigned int > >("num_elements")[0],
-       const size_t num_refinements = default_config().get< size_t >("num_refinements"),
+  explicit Cube(const DomainFieldType lower_left = default_config().get< DomainType >("lower_left")[0],
+                const DomainFieldType upper_right = default_config().get< DomainType >("upper_right")[0],
+                const unsigned int num_elements = default_config().get< std::vector< unsigned int > >("num_elements")[0],
+                const size_t num_refinements = default_config().get< size_t >("num_refinements"),
                 const std::array< unsigned int, dimDomain > overlap
-                = DSC::make_array< unsigned int, dimDomain >(default_config().get< unsigned int >("overlap")))
+                    = DSC::make_array< unsigned int, dimDomain >(default_config().get< unsigned int >("overlap")))
     : grid_ptr_(create_grid(DomainType(lower_left),
                             DomainType(upper_right),
                             parse_array(num_elements),
@@ -207,7 +207,7 @@ public:
        const unsigned int num_elements = default_config().get< std::vector< unsigned int > >("num_elements")[0],
        const size_t num_refinements = default_config().get< size_t >("num_refinements"),
        const std::array< unsigned int, dimDomain > overlap
-       = DSC::make_array< unsigned int, dimDomain >(default_config().get< unsigned int >("overlap")))
+          = DSC::make_array< unsigned int, dimDomain >(default_config().get< unsigned int >("overlap")))
     : grid_ptr_(create_grid(lower_left, upper_right, parse_array(num_elements), num_refinements))
   {}
 
